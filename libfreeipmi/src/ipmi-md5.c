842800ca51196ffd46fb44f05ea619375bb3f358
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2004-12-17 02:14:56 +0000

    fix headers

--- libfreeipmi/src/ipmi-md5.c
+++ libfreeipmi/src/ipmi-md5.c
@@ -1,22 +1,28 @@
-/* 
-   ipmi-md5.c - IPMI Command Specification
-
-   Copyright (C) 2003 FreeIPMI Core Team
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
-*/
+/*****************************************************************************\
+ *  $Id: ipmi-md5.c,v 1.3 2004-12-17 02:14:56 chu11 Exp $
+ *****************************************************************************
+ *  Copyright (C) 2003 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Albert Chu <chu11@llnl.gov>
+ *  UCRL-CODE-155698
+ *
+ *  This file is part of Ipmipower, a remote power control utility.
+ *  For details, see http://www.llnl.gov/linux/.
+ *
+ *  Ipmipower is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation; either version 2 of the License, or (at your
+ *  option) any later version.
+ *
+ *  Ipmipower is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Ipmipower; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
