fccc077eadf32fd5675e4de74fe362c878922c73
ab <ab@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2005-04-26 07:41:15 +0000

    replaced error with perror

--- fish/src/xmalloc.h
+++ fish/src/xmalloc.h
@@ -56,8 +56,6 @@ void free ();
 # define _(Text) Text
 #endif
 
-#include "error.h"
-
 #ifndef EXIT_FAILURE
 # define EXIT_FAILURE 1
 #endif
@@ -69,12 +67,6 @@ __VOID *xcalloc (size_t n, size_t s);
 __VOID *xrealloc (__VOID *p, size_t n);
 char *xstrdup (char *p);
 void xfree (__VOID *p);
-#endif
-
-#if __STDC__ && (HAVE_VPRINTF || HAVE_DOPRNT)
-void error (int, int, const char *, ...);
-#else
-void error ();
 #endif
 
 #ifdef __cplusplus
