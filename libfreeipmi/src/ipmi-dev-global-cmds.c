7586e27d5fec51f390c8c02d3f483981e3aa1dd6
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2006-01-30 15:53:42 +0000

    add parameter check

--- libfreeipmi/src/ipmi-dev-global-cmds.c
+++ libfreeipmi/src/ipmi-dev-global-cmds.c
@@ -88,6 +88,12 @@ fiid_template_t tmpl_cmd_get_dev_id_sr870bn4_rs =
 int8_t 
 fill_cmd_get_dev_id (fiid_obj_t obj_data_rq)
 { 
+  if (!obj_data_rq)
+    {
+      errno = EINVAL;
+      return (-1);
+    }
+
   FIID_OBJ_SET (obj_data_rq, tmpl_cmd_get_dev_id_rq, (uint8_t *)"cmd", IPMI_CMD_GET_DEV_ID);
   return (0);
 }
