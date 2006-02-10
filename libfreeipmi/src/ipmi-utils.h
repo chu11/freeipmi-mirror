dc9e7183d21ad9fbbbcf15834f454ad2ee67ae8c
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2006-02-10 18:10:43 +0000

    more code cleanup

--- libfreeipmi/src/ipmi-utils.h
+++ libfreeipmi/src/ipmi-utils.h
@@ -55,11 +55,6 @@ int8_t ipmi_chksum (uint8_t *buf, uint64_t len);
 int8_t ipmi_chksum_test (uint8_t *buf, uint64_t len);
 int8_t ipmi_comp_test (fiid_obj_t obj_cmd);
 int ipmi_open_free_udp_port (void);
-int ipmi_ioremap (uint64_t physical_addr, size_t physical_addr_len, void **virtual_addr, void **mapped_addr, size_t *mapped_addr_len);
-int ipmi_iounmap (void *mapped_addr, size_t mapped_addr_len);
-int ipmi_get_physical_mem_data (uint64_t physical_address, size_t length, uint8_t *data);
-
-
 
 #ifdef __cplusplus
 }
