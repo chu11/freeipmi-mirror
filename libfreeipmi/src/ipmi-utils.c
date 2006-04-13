c20775f1d7ddbb9ca687c4f9ab71117129f81679
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2006-04-13 20:46:43 +0000

    gcrypt->libgcrypt

--- libfreeipmi/src/ipmi-utils.c
+++ libfreeipmi/src/ipmi-utils.c
@@ -121,7 +121,6 @@ ipmi_check_completion_code(fiid_obj_t obj_cmd, uint8_t completion_code)
                 "ipmi_check_completion_code: completion code invalid: %x",
                 (uint8_t)completion_code_recv);
       syslog (LOG_MAKEPRI (LOG_FAC (LOG_LOCAL1), LOG_ERR), errstr);
-    }
 #endif /* IPMI_SYSLOG */
 #if defined (IPMI_TRACE)
       fprintf(stderr,
