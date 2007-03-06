5ea6b8fdb8375d2779008af343bbe814cf9e123c
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2007-03-06 22:39:28 +0000

    add -N option

--- ipmiconsole/src/ipmiconsole/ipmiconsole_config.c
+++ ipmiconsole/src/ipmiconsole/ipmiconsole_config.c
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  $Id: ipmiconsole_config.c,v 1.2 2006-12-08 01:10:47 chu11 Exp $
+ *  $Id: ipmiconsole_config.c,v 1.3 2007-03-06 22:39:28 chu11 Exp $
  *****************************************************************************
  *  Copyright (C) 2006 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
@@ -83,6 +83,7 @@ _usage(void)
 	  "-l --privilege str            Privilege\n"
 	  "-c --cipher-suite-id num      Cipher Suite Privilege\n"
           "-C --config                   Select alternate config file\n"
+          "-N --notsteal                 Do not steal in use SOL sessions by default\n"
           "-I --intel-2-0-session        Workaround Intel IPMI bugs\n"
           "-S --supermicro-2-0-session   Workaround Supermicro IPMI bugs\n");
 #ifndef NDEBUG
@@ -122,6 +123,7 @@ _cmdline_parse(int argc, char **argv)
       {"privilege",                1, NULL, 'l'},
       {"cipher-suite-id",          1, NULL, 'c'},
       {"config-file",              1, NULL, 'C'}, 
+      {"not-steal",                0, NULL, 'N'},
       {"intel-2-0-session",        0, NULL, 'I'},
       {"supermicro-2-0-session",   0, NULL, 'S'},
 #ifndef NDEBUG
@@ -138,7 +140,7 @@ _cmdline_parse(int argc, char **argv)
   assert(conf);
 
   memset(options, '\0', sizeof(options));
-  strcat(options, "HVh:u:p:Pk:l:c:C:IS");
+  strcat(options, "HVh:u:p:Pk:l:c:C:NIS");
 #ifndef NDEBUG
   strcat(options, "DEFG");
 #endif /* NDEBUG */
@@ -221,6 +223,10 @@ _cmdline_parse(int argc, char **argv)
 	  if (!(conf->config_file = strdup(optarg)))
 	    err_exit("strdup: %s", strerror(errno));
 	  break;
+        case 'N':       /* --not-steal */
+          conf->not_steal++;
+          conf->not_steal_set++;
+          break;
         case 'I':       /* --intel-2-0-session */
           conf->intel_2_0_session++;
           conf->intel_2_0_session_set++;
@@ -404,6 +410,7 @@ _config_file_parse(void)
     k_g_flag,
     privilege_flag, 
     cipher_suite_id_flag,
+    not_steal_flag,
     intel_2_0_session_flag,
     supermicro_2_0_session_flag;
   
@@ -474,6 +481,17 @@ _config_file_parse(void)
         &cipher_suite_id_flag,
         NULL, 
         0
+      },
+      {
+        "not-steal", 
+        CONFFILE_OPTION_BOOL, 
+        -1, 
+        _cb_bool,
+        1, 
+        0, 
+        &not_steal_flag, 
+        &(conf->not_steal),
+        conf->not_steal_set
       },
       {
         "intel_2_0_session", 
--- ipmiconsole/src/ipmiconsole/ipmiconsole_config.h
+++ ipmiconsole/src/ipmiconsole/ipmiconsole_config.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  $Id: ipmiconsole_config.h,v 1.1 2006-11-06 00:13:12 chu11 Exp $
+ *  $Id: ipmiconsole_config.h,v 1.2 2007-03-06 22:39:28 chu11 Exp $
  *****************************************************************************
  *  Copyright (C) 2006 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
@@ -56,6 +56,7 @@ struct ipmiconsole_config
   char k_g[IPMI_MAX_K_G_LENGTH+1];
   int privilege;
   int cipher_suite_id;
+  int not_steal;
   int intel_2_0_session;
   int supermicro_2_0_session;
 
@@ -65,6 +66,7 @@ struct ipmiconsole_config
   int k_g_set;
   int privilege_set;
   int cipher_suite_id_set;
+  int not_steal_set;
   int intel_2_0_session_set;
   int supermicro_2_0_session_set;
 };
