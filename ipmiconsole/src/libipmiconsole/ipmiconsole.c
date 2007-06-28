6a6f212b4c49ebf5d15b1c9b22e5c45522ff703e
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2007-06-28 22:23:48 +0000

    fix several locking corner cases/bugs

--- ipmiconsole/src/libipmiconsole/ipmiconsole.c
+++ ipmiconsole/src/libipmiconsole/ipmiconsole.c
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  $Id: ipmiconsole.c,v 1.12 2007-06-28 22:16:15 chu11 Exp $
+ *  $Id: ipmiconsole.c,v 1.13 2007-06-28 22:23:48 chu11 Exp $
  *****************************************************************************
  *  Copyright (C) 2006 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
@@ -217,6 +217,8 @@ ipmiconsole_engine_submit(ipmiconsole_ctx_t c)
   if (c->session_submitted)
     {
       c->errnum = IPMICONSOLE_ERR_CTX_ALREADY_SUBMITTED;
+      if ((rv = pthread_mutex_unlock(&(c->session_submitted_mutex))))
+        IPMICONSOLE_DEBUG(("pthread_mutex_unlock: %s", strerror(rv)));
       return -1;
     }
 
@@ -338,6 +340,8 @@ ipmiconsole_engine_submit_block(ipmiconsole_ctx_t c)
   if (c->session_submitted)
     {
       c->errnum = IPMICONSOLE_ERR_CTX_ALREADY_SUBMITTED;
+      if ((rv = pthread_mutex_unlock(&(c->session_submitted_mutex))))
+        IPMICONSOLE_DEBUG(("pthread_mutex_unlock: %s", strerror(rv)));
       return -1;
     }
 
@@ -693,12 +697,30 @@ ipmiconsole_ctx_generate_break(ipmiconsole_ctx_t c)
 int
 ipmiconsole_ctx_destroy(ipmiconsole_ctx_t c)
 {
+  int rv;
+
   if (!c || c->magic != IPMICONSOLE_CTX_MAGIC)
     return -1;
   
+  if ((rv = pthread_mutex_lock(&(c->session_submitted_mutex))))
+    {
+      IPMICONSOLE_DEBUG(("pthread_mutex_lock: %s", strerror(rv)));
+      c->errnum = IPMICONSOLE_ERR_INTERNAL;
+      return -1;
+    }
+
   if (c->session_submitted)
     {
       c->errnum = IPMICONSOLE_ERR_CTX_IS_SUBMITTED;
+      if ((rv = pthread_mutex_unlock(&(c->session_submitted_mutex))))
+        IPMICONSOLE_DEBUG(("pthread_mutex_unlock: %s", strerror(rv)));
+      return -1;
+    }
+
+  if ((rv = pthread_mutex_unlock(&(c->session_submitted_mutex))))
+    {
+      IPMICONSOLE_DEBUG(("pthread_mutex_unlock: %s", strerror(rv)));
+      c->errnum = IPMICONSOLE_ERR_INTERNAL;
       return -1;
     }
 
