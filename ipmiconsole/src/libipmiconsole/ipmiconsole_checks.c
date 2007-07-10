4fa4e9d835363fb287bf73f693be5531e4333ba1
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2007-07-10 18:30:13 +0000

    add comment

--- ipmiconsole/src/libipmiconsole/ipmiconsole_checks.c
+++ ipmiconsole/src/libipmiconsole/ipmiconsole_checks.c
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  $Id: ipmiconsole_checks.c,v 1.6 2007-07-10 18:15:47 chu11 Exp $
+ *  $Id: ipmiconsole_checks.c,v 1.7 2007-07-10 18:30:10 chu11 Exp $
  *****************************************************************************
  *  Copyright (C) 2006 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
@@ -164,6 +164,18 @@ ipmiconsole_check_outbound_sequence_number(ipmiconsole_ctx_t c, ipmiconsole_pack
 	 || p == IPMICONSOLE_PACKET_TYPE_CLOSE_SESSION_RS);
   
   s = &(c->session);
+
+  /* achu: This algorithm is more or less from Appendix A of the IPMI
+   * spec.  It may not be entirely necessary for ipmipower, since the
+   * requester sequence number puts packets into lock-step mode.  Oh
+   * well.
+   *
+   * I know that technically I could remove a lot of code here if I
+   * just let unsigned ints be unsigned ints (i.e. 0x00 - 0xff = 1).
+   * I dunno, I like to see all of the code actually written out b/c
+   * it makes more sense to the casual code reviewer.  Maybe I'll
+   * change it later.
+   */
 
   if (Fiid_obj_get(c,
 		   s->obj_rmcpplus_session_hdr_rs,
