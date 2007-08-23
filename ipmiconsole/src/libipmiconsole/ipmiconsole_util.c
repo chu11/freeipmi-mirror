ceef09d684503604fda4e5b4b35e95491c3125af
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2007-08-23 15:46:50 +0000

    add forgotten files

---
+++ ipmiconsole/src/libipmiconsole/ipmiconsole_util.c
@@ -0,0 +1,76 @@
+/*****************************************************************************\
+ *  $Id: ipmiconsole_util.c,v 1.1 2007-08-23 15:46:50 chu11 Exp $
+ *****************************************************************************
+ *  Copyright (C) 2006 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Albert Chu <chu11@llnl.gov>
+ *  UCRL-CODE-221226
+ *  
+ *  This file is part of Ipmiconsole, a set of IPMI 2.0 SOL libraries
+ *  and utilities.  For details, see http://www.llnl.gov/linux/.
+ *  
+ *  Ipmiconsole is free software; you can redistribute it and/or modify 
+ *  it under the terms of the GNU General Public License as published by the 
+ *  Free Software Foundation; either version 2 of the License, or (at your 
+ *  option) any later version.
+ *  
+ *  Ipmiconsole is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ *  for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with Ipmiconsole; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <stdlib.h>
+#if STDC_HEADERS
+#include <string.h>
+#endif /* STDC_HEADERS */
+#if HAVE_UNISTD_H
+#include <unistd.h>
+#endif /* HAVE_UNISTD_H */
+#if HAVE_FCNTL_H
+#include <fcntl.h>
+#endif /* HAVE_FCNTL_H */
+#include <assert.h>
+#include <errno.h>
+
+#include "ipmiconsole.h"
+#include "ipmiconsole_defs.h"
+
+#include "list.h"
+#include "ipmiconsole_debug.h"
+#include "ipmiconsole_engine.h"
+
+int 
+ipmiconsole_set_closeonexec(ipmiconsole_ctx_t c, int fd)
+{
+  int closeonexec;
+
+  /* User need not pass in valid context for this function */
+
+  if ((closeonexec = fcntl(fd, F_GETFD, 0)) < 0)
+    {
+      IPMICONSOLE_DEBUG(("fcntl: %s", strerror(errno)));
+      if (c && c->magic == IPMICONSOLE_CTX_MAGIC)
+        c->errnum = IPMICONSOLE_ERR_SYSTEM_ERROR;
+      return -1;
+    }
+  closeonexec |= FD_CLOEXEC;
+  if (fcntl(fd, F_SETFD, closeonexec) < 0)
+    {
+      IPMICONSOLE_DEBUG(("fcntl: %s", strerror(errno)));
+      if (c && c->magic == IPMICONSOLE_CTX_MAGIC)
+        c->errnum = IPMICONSOLE_ERR_SYSTEM_ERROR;
+      return -1;
+    }
+  
+  return 0;
+}
---
+++ ipmiconsole/src/libipmiconsole/ipmiconsole_util.h
@@ -0,0 +1,34 @@
+/*****************************************************************************\
+ *  $Id: ipmiconsole_util.h,v 1.1 2007-08-23 15:46:50 chu11 Exp $
+ *****************************************************************************
+ *  Copyright (C) 2006 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Albert Chu <chu11@llnl.gov>
+ *  UCRL-CODE-221226
+ *  
+ *  This file is part of Ipmiconsole, a set of IPMI 2.0 SOL libraries
+ *  and utilities.  For details, see http://www.llnl.gov/linux/.
+ *  
+ *  Ipmiconsole is free software; you can redistribute it and/or modify 
+ *  it under the terms of the GNU General Public License as published by the 
+ *  Free Software Foundation; either version 2 of the License, or (at your 
+ *  option) any later version.
+ *  
+ *  Ipmiconsole is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ *  for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with Ipmiconsole; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifndef _IPMICONSOLE_UTIL_H
+#define _IPMICONSOLE_UTIL_H
+
+#include "ipmiconsole.h"
+
+int ipmiconsole_set_closeonexec(ipmiconsole_ctx_t c, int fd);
+
+#endif /* _IPMICONSOLE_UTIL_H */
