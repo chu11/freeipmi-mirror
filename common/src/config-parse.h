eeaa83525a7929984a1698f432fe5f18b291117e
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2007-09-17 05:27:33 +0000

    continue work

--- common/src/config-parse.h
+++ common/src/config-parse.h
@@ -23,7 +23,35 @@
 #ifndef _CONFIG_PARSE_H_
 #define _CONFIG_PARSE_H_
 
+#include <stdio.h>
+
 #include "config-parse.h"
+
+#define CONFIG_CHECKOUT_KEY_COMMENTED_OUT                  0x01
+#define CONFIG_CHECKOUT_KEY_COMMENTED_OUT_IF_VALUE_EMPTY   0x02
+#define CONFIG_DO_NOT_CHECKOUT                             0x04
+
+typedef enum
+  {
+    CONFIG_ERR_FATAL_ERROR = -2,
+    CONFIG_ERR_NON_FATAL_ERROR = -1,
+    CONFIG_ERR_SUCCESS = 0,
+  } config_err_t;
+
+typedef enum
+  {
+    CONFIG_DIFF_FATAL_ERROR = -2,
+    CONFIG_DIFF_NON_FATAL_ERROR = -1,
+    CONFIG_DIFF_SAME = 0,
+    CONFIG_DIFF_DIFFERENT = 1,
+  } config_diff_t;
+
+typedef enum
+  {
+    CONFIG_VALIDATE_FATAL_ERROR = -2,
+    CONFIG_VALIDATE_INVALID_VALUE = -1,
+    CONFIG_VALIDATE_VALID_VALUE = 0,
+  } config_validate_t;
 
 struct config_key
 {
@@ -39,16 +67,36 @@ struct config_keyvalue {
   struct config_keyvalue *next;
 };
 
+/* Output a comment/instructions for a particular section */
+typedef config_err_t (*Section_Comment) (const char *section_name,
+                                         FILE *fp);
+
+/* checkout procedure fills the value into kv->value as printable string */
+typedef config_err_t (*Section_Checkout) (const char *section_name,
+                                          FILE *fp);
+
+/* commit procedure takes string value from kv->value and converts and
+   does ipmi calls to set it */
+typedef config_err_t (*Section_Commit) (const char *section_name,
+                                        struct config_keyvalue *keyvalues);
+
+/* diff procedure finds the difference with the ipmi actual value
+   and kv->value */
+typedef config_diff_t (*Section_Diff) (const char *section_name,
+                                       struct config_keyvalue *keyvalues);
+
+/* validate procedure finds if value is suitable to be set as kv->value */
+typedef config_validate_t (*Section_Validate) (const char *section_name,
+                                               struct config_keyvalue *keyvalues);
+
 struct config_section {
   char *section_name;
   unsigned int flags;
-#if 0
   Section_Comment comment; 
   Section_Checkout checkout;
   Section_Commit commit;
   Section_Diff diff;
   Section_Validate validate;
-#endif
   /* keys in this section */
   struct config_key *keys;
   /* key and values read from the file */
