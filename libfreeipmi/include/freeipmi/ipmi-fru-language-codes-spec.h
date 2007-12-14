93d0da70d5cd85e7ae260bd9c1b6b06b3bfc8c6d
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2007-12-14 04:31:51 +0000

    ...

--- libfreeipmi/include/freeipmi/ipmi-fru-language-codes-spec.h
+++ libfreeipmi/include/freeipmi/ipmi-fru-language-codes-spec.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  $Id: ipmi-fru-language-codes-spec.h,v 1.1.2.1 2007-12-14 00:32:24 chu11 Exp $
+ *  $Id: ipmi-fru-language-codes-spec.h,v 1.1.2.2 2007-12-14 04:31:51 chu11 Exp $
  *****************************************************************************
  *  Copyright (C) 2007 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
@@ -40,40 +40,6 @@ extern "C" {
  * Not in IPMI spec.  In "Platform Management FRU Information Storage
  * Definition" document.
  */
-
-#define IPMI_FRU_COMMON_HEADER_FORMAT_VERSION         0x01
-#define IPMI_FRU_CHASSIS_INFO_AREA_FORMAT_VERSION     0x01
-#define IPMI_FRU_BOARD_INFO_AREA_FORMAT_VERSION       0x01
-#define IPMI_FRU_PRODUCT_INFO_AREA_FORMAT_VERSION     0x01
-#define IPMI_FRU_MULTIRECORD_INFO_AREA_FORMAT_VERSION 0x02
-
-#define IPMI_FRU_CHASSIS_TYPE_OTHER                 0x01
-#define IPMI_FRU_CHASSIS_TYPE_UNKNOWN               0x02
-#define IPMI_FRU_CHASSIS_TYPE_DESKTOP               0x03
-#define IPMI_FRU_CHASSIS_TYPE_LOW_PROFILE_DESKTOP   0x04
-#define IPMI_FRU_CHASSIS_TYPE_PIZZA_BOX             0x05
-#define IPMI_FRU_CHASSIS_TYPE_MINI_TOWER            0x06
-#define IPMI_FRU_CHASSIS_TYPE_TOWER                 0x07
-#define IPMI_FRU_CHASSIS_TYPE_PORTABLE              0x08
-#define IPMI_FRU_CHASSIS_TYPE_LAPTOP                0x09
-#define IPMI_FRU_CHASSIS_TYPE_NOTEBOOK              0x0a
-#define IPMI_FRU_CHASSIS_TYPE_HAND_HELD             0x0b
-#define IPMI_FRU_CHASSIS_TYPE_DOCKING_STATION       0x0c
-#define IPMI_FRU_CHASSIS_TYPE_ALL_IN_ONE            0x0d
-#define IPMI_FRU_CHASSIS_TYPE_SUB_NOTEBOOK          0x0e
-#define IPMI_FRU_CHASSIS_TYPE_SPACE_SAVING          0x0f
-#define IPMI_FRU_CHASSIS_TYPE_LUNCH_BOX             0x10
-#define IPMI_FRU_CHASSIS_TYPE_MAIN_SERVER_CHASSIS   0x11
-#define IPMI_FRU_CHASSIS_TYPE_EXPANSION_CHASSIS     0x12
-#define IPMI_FRU_CHASSIS_TYPE_SUBCHASSIS            0x13
-#define IPMI_FRU_CHASSIS_TYPE_BUS_EXPANSION_CHASSIS 0x14
-#define IPMI_FRU_CHASSIS_TYPE_PERIPHERAL_CHASSIS    0x15
-#define IPMI_FRU_CHASSIS_TYPE_RAID_CHASSIS          0x16
-#define IPMI_FRU_CHASSIS_TYPE_RACK_MOUNT_CHASSIS    0x17
-
-#define IPMI_FRU_CHASSIS_TYPE_VALID(__chassis_type) \
-        (((__chassis_type) >= IPMI_FRU_CHASSIS_TYPE_OTHER \
-          && (__chassis_type) <= IPMI_FRU_CHASSIS_TYPE_RACK_MOUNT_CHASSIS) ? 1 : 0)
 
 #define IPMI_FRU_LANGUAGE_CODE_ENGLISH_LEGACY       0
 #define IPMI_FRU_LANGUAGE_CODE_AFAR                 1
@@ -217,73 +183,7 @@ extern "C" {
         (((__language_code) >= IPMI_FRU_LANGUAGE_CODE_ENGLISH_LEGACY \
           && (__language_code) <= IPMI_FRU_LANGUAGE_CODE_ZULU) ? 1 : 0)
 
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_POWER_SUPPLY_INFORMATION       0x00
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_DC_OUTPUT                      0x01
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_DC_LOAD                        0x02
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_MANAGEMENT_ACCESS_RECORD       0x03
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_BASE_COMPATIBILITY_RECORD      0x04
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_EXTENDED_COMPATIBILITY_RECORD  0x05
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_OEM_MIN                        0xC0
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_OEM_MAX                        0xFF
-
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_VALID(__multirecord_area_type) \
-        (((__multirecord_area_type) >= IPMI_FRU_MULTIRECORD_AREA_TYPE_POWER_SUPPLY_INFORMATION \
-          && (__multirecord_area_type) <= IPMI_FRU_MULTIRECORD_AREA_TYPE_EXTENDED_COMPATIBILITY_RECORD) ? 1 : 0)
-
-#define IPMI_FRU_MULTIRECORD_AREA_TYPE_IS_OEM(__multirecord_area_type) \
-        (((__multirecord_area_type) >= IPMI_FRU_MULTIRECORD_AREA_TYPE_OEM_MIN \
-          && (__multirecord_area_type) <= IPMI_FRU_MULTIRECORD_AREA_TYPE_OEM_MAX) ? 1 : 0)
-
-#define IPMI_FRU_ONE_PULSE_PER_ROTATION  0x0
-#define IPMI_FRU_TWO_PULSES_PER_ROTATION 0x1
-
-#define IPMI_FRU_VOLTAGE_12V      0x0
-#define IPMI_FRU_VOLTAGE_MINUS12V 0x1
-#define IPMI_FRU_VOLTAGE_5V       0x2
-#define IPMI_FRU_VOLTAGE_3_3V     0x3
-
-#define IPMI_FRU_PREDICTIVE_FAIL_PIN_IS_PASS_FAIL 0x0
-
-#define IPMI_FRU_SUB_RECORD_TYPE_SYSTEM_MANAGEMENT_URL     0x01
-#define IPMI_FRU_SUB_RECORD_TYPE_SYSTEM_NAME               0x02
-#define IPMI_FRU_SUB_RECORD_TYPE_SYSTEM_PING_ADDRESS       0x03
-#define IPMI_FRU_SUB_RECORD_TYPE_COMPONENT_MANAGEMENT_URL  0x04
-#define IPMI_FRU_SUB_RECORD_TYPE_COMPONENT_NAME            0x05
-#define IPMI_FRU_SUB_RECORD_TYPE_COMPONENT_PING_ADDRESS    0x06
-#define IPMI_FRU_SUB_RECORD_TYPE_SYSTEM_UNIQUE_ID          0x07
-
-#define IPMI_FRU_SUB_RECORD_TYPE_VALID(__sub_record_type) \
-        (((__sub_record_type) >= IPMI_FRU_SUB_RECORD_TYPE_SYSTEM_MANAGEMENT_URL \
-          && (__sub_record_type) <= IPMI_FRU_SUB_RECORD_TYPE_SYSTEM_UNIQUE_ID) ? 1 : 0)
-
-#define IPMI_FRU_TYPE_LENGTH_TYPE_CODE_BINARY          0x00
-#define IPMI_FRU_TYPE_LENGTH_TYPE_CODE_BCD             0x40
-#define IPMI_FRU_TYPE_LENGTH_TYPE_CODE_SIXBIT_ASCII    0x80
-#define IPMI_FRU_TYPE_LENGTH_TYPE_CODE_LANGUAGE_CODE   0xC0
-#define IPMI_FRU_TYPE_LENGTH_TYPE_CODE_MASK            0xC0
-#define IPMI_FRU_TYPE_LENGTH_NUMBER_OF_DATA_BYTES_MASK 0x1F
-
-#define IPMI_FRU_BCD_DIGIT_MIN                         0x0
-#define IPMI_FRU_BCD_DIGIT_MAX                         0x9
-#define IPMI_FRU_BCD_SPACE                             0xA
-#define IPMI_FRU_BCD_DASH                              0xB
-#define IPMI_FRU_BCD_PERIOD                            0xC
-
-#define IPMI_FRU_SENTINEL_VALUE                        0xC1
-
-extern const char *const ipmi_fru_chassis_types[];
 extern const char *const ipmi_fru_language_codes[];
-
-extern fiid_template_t tmpl_fru_common_header;
-extern fiid_template_t tmpl_fru_info_area_header;
-extern fiid_template_t tmpl_fru_multirecord_area_header;
-extern fiid_template_t tmpl_fru_power_supply_information;
-extern fiid_template_t tmpl_fru_dc_output;
-extern fiid_template_t tmpl_fru_dc_load;
-extern fiid_template_t tmpl_fru_management_access_record;
-extern fiid_template_t tmpl_fru_base_compatibility_record;
-extern fiid_template_t tmpl_fru_extended_compatibility_record;
-extern fiid_template_t tmpl_fru_oem_record;
 
 #ifdef __cplusplus
 }
