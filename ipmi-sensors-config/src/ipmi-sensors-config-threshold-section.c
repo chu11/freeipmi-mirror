ec95c7fc4b1ed1837d96bff81653038db97d4ac1
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2008-02-12 22:39:32 +0000

    sensors-config wip

--- ipmi-sensors-config/src/ipmi-sensors-config-threshold-section.c
+++ ipmi-sensors-config/src/ipmi-sensors-config-threshold-section.c
@@ -15,83 +15,6 @@
 #include "tool-sdr-cache-common.h"
 
 static config_err_t
-_get_sdr_record (ipmi_sensors_config_state_data_t *state_data,
-                 const char *section_name,
-                 uint8_t *sdr_record,
-                 unsigned int *sdr_record_len)
-{
-  uint16_t record_id;
-  char *str = NULL;
-  char *ptr;
-  config_err_t rv = CONFIG_ERR_FATAL_ERROR;
-  int len;
-
-  assert(state_data);
-  assert(section_name);
-  assert(sdr_record);
-  assert(sdr_record_len);
-
-  if (!(str = strdup(section_name)))
-    {
-      if (state_data->prog_data->args->config_args.common.flags & IPMI_FLAGS_DEBUG_DUMP)
-        perror("strdup");
-      goto cleanup;
-    }
-
-  if (!(ptr = strchr(str, '_')))
-    {
-      if (state_data->prog_data->args->config_args.common.flags & IPMI_FLAGS_DEBUG_DUMP)
-        fprintf(stderr,
-                "Invalid section_name: %s\n",
-                section_name);
-      goto cleanup;
-    }
-
-  *ptr = '\0';
-
-  record_id = strtoul(str, &ptr,0);
-  if (*ptr != '\0')
-    {
-      if (state_data->prog_data->args->config_args.common.flags & IPMI_FLAGS_DEBUG_DUMP)
-        fprintf(stderr,
-                "Invalid section_name: %s\n",
-                section_name);
-      goto cleanup;
-    }
-    
-  if (ipmi_sdr_cache_search_record_id(state_data->ipmi_sdr_cache_ctx,
-                                      record_id) < 0)
-    {
-      if (state_data->prog_data->args->config_args.common.flags & IPMI_FLAGS_DEBUG_DUMP)
-        fprintf(stderr,
-                "Record_id not found: %u\n",
-                record_id);
-      rv = CONFIG_ERR_NON_FATAL_ERROR;
-      goto cleanup;
-    }
-
-  if ((len = ipmi_sdr_cache_record_read(state_data->ipmi_sdr_cache_ctx,
-                                        sdr_record,
-                                        *sdr_record_len)) < 0)
-    {
-      if (state_data->prog_data->args->config_args.common.flags & IPMI_FLAGS_DEBUG_DUMP)
-        fprintf(stderr,
-                "ipmi_sdr_cache_record_read: %s\n",
-                ipmi_sdr_cache_ctx_strerror(ipmi_sdr_cache_ctx_errnum(state_data->ipmi_sdr_cache_ctx)));
-      rv = CONFIG_ERR_NON_FATAL_ERROR;
-      goto cleanup;
-    }
-
-  *sdr_record_len = len;
-
-  rv = CONFIG_ERR_SUCCESS;
- cleanup:
-  if (str)
-    free(str);
-  return rv;
-}
-
-static config_err_t
 _calculate_threshold(ipmi_sensors_config_state_data_t *state_data,
                      uint8_t *sdr_record,
                      unsigned int sdr_record_len,
@@ -180,10 +103,10 @@ threshold_checkout (const char *section_name,
   double threshold_calc;
   uint8_t sensor_number;
 
-  if ((ret = _get_sdr_record(state_data,
-                             section_name,
-                             sdr_record,
-                             &sdr_record_len)) != CONFIG_ERR_SUCCESS)
+  if ((ret = get_sdr_record(state_data,
+                            section_name,
+                            sdr_record,
+                            &sdr_record_len)) != CONFIG_ERR_SUCCESS)
     {
       rv = ret;
       goto cleanup;
--- ipmi-sensors-config/src/ipmi-sensors-config-utils.h
+++ ipmi-sensors-config/src/ipmi-sensors-config-utils.h
@@ -6,4 +6,9 @@
 config_err_t convert_id_string (ipmi_sensors_config_state_data_t *state_data, 
                                 char *id_string);
 
+config_err_t get_sdr_record (ipmi_sensors_config_state_data_t *state_data,
+                             const char *section_name,
+                             uint8_t *sdr_record,
+                             unsigned int *sdr_record_len);
+
 #endif
