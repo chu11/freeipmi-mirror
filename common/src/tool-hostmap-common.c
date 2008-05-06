07eba6c69c06750db6e09bd8531ac685e50ecbba
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2008-05-06 22:55:49 +0000

    new lib beginning

---
+++ common/src/tool-hostmap-common.c
@@ -0,0 +1,43 @@
+/*
+  Copyright (C) 2003-2008 FreeIPMI Core Team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+  
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA
+*/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#if STDC_HEADERS
+#include <string.h>
+#endif
+#include <errno.h>
+#include <assert.h>
+
+#include "freeipmi-portability.h"
+
+#include "conffile.h"
+
+struct hostmap
+{
+  int32_t magic;
+};
+
+hostmap_t
+hostmap_parse()
+{
+}
---
+++ common/src/tool-hostmap-common.h
@@ -0,0 +1,24 @@
+/*
+  Copyright (C) 2003-2008 FreeIPMI Core Team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+  
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA
+*/
+
+#ifndef _TOOL_HOSTMAP_COMMON_H
+#define _TOOL_HOSTMAP_COMMON_H
+
+typedef struct hostmap *hostmap_t;
+
+#endif
