0d995ac5f9e13ad45aed63b0fa763fba4006fe54
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2008-08-01 21:29:11 +0000

    fix typos

--- pef-config/src/pef-config.c
+++ pef-config/src/pef-config.c
@@ -267,7 +267,7 @@ _pef_config (pstdout_state_t pstate,
                                                    &(prog_data->args->config_args),
                                                    1,
                                                    fp,
-                                                   0.
+                                                   0,
                                                    &state_data);
                 if (this_ret != CONFIG_ERR_SUCCESS)
                   ret = this_ret;
@@ -289,7 +289,7 @@ _pef_config (pstdout_state_t pstate,
                                    &(prog_data->args->config_args),
                                    all_keys_if_none_specified,
                                    fp,
-                                   0
+                                   0,
                                    &state_data);
           }
         break;
