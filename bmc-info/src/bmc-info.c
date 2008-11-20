c9ee233d562f0d64ebbb918dcf306990b754a0d9
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2008-11-20 17:11:04 +0000

    comment on aux info

--- bmc-info/src/bmc-info.c
+++ bmc-info/src/bmc-info.c
@@ -263,6 +263,7 @@ display_get_device_id (bmc_info_state_data_t *state_data)
     _FIID_OBJ_GET (cmd_rs, "product_id", &product_id);
     pstdout_printf(state_data->pstate, "Product ID:        %Xh\n", (unsigned int) product_id);
     
+    /* auxiliary firmware info is optional */
     _FIID_OBJ_GET_WITH_RETURN_VALUE (cmd_rs,
                                      "auxiliary_firmware_revision_info",
                                      &val,
