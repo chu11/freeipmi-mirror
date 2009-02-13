86858184e327dea96137c78bfce112d962c5b2b7
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-02-13 18:59:56 +0000

    add new files

--- libfreeipmi/src/libcommon/ipmi-trace.h
+++ libfreeipmi/src/libcommon/ipmi-trace.h
@@ -37,56 +37,64 @@ extern "C" {
 
 #include "freeipmi/fiid/fiid.h"
 
+#include "ipmi-fiid-util.h"
+
 #define ERR_WRAPPER_STR_MAX_LEN 4096
 
 #if defined (IPMI_TRACE)
-#define __MSG_TRACE(__msgtracestr, __msgtracenum)                       \
-do {                                                                    \
-  fprintf (stderr,                                                      \
-           "%s: %d: %s: error '%s' (%d)\n",                             \
-           __FILE__, __LINE__, __PRETTY_FUNCTION__,                     \
-           __msgtracestr, __msgtracenum);                               \
-  fflush (stderr);                                                      \
+#define __MSG_TRACE(__msgtracestr, __msgtracenum)                          \
+do {                                                                       \
+  fprintf (stderr,                                                         \
+           "%s: %d: %s: error '%s' (%d)\n",                                \
+           __FILE__, __LINE__, __PRETTY_FUNCTION__,                        \
+           __msgtracestr, __msgtracenum);                                  \
+  fflush (stderr);                                                         \
 } while (0)
 
-#define __ERRNO_TRACE(__errno_orig)                                     \
-do {                                                                    \
-  extern int errno;                                                     \
-  int __save_errno = __errno_orig;                                      \
-  char __errnostr[ERR_WRAPPER_STR_MAX_LEN];                             \
-  memset (__errnostr, '\0', ERR_WRAPPER_STR_MAX_LEN);                   \
-  strerror_r(__save_errno, __errnostr, ERR_WRAPPER_STR_MAX_LEN);        \
-  fprintf (stderr,                                                      \
-           "%s: %d: %s: errno '%s' (%d)\n",                             \
-           __FILE__, __LINE__, __PRETTY_FUNCTION__,                     \
-           __errnostr, __save_errno);                                   \
-  fflush (stderr);                                                      \
-  __errno_orig = __save_errno;                                          \
+#define __ERRNO_TRACE(__errno_orig)                                        \
+do {                                                                       \
+  extern int errno;                                                        \
+  int __save_errno = __errno_orig;                                         \
+  char __errnostr[ERR_WRAPPER_STR_MAX_LEN];                                \
+  memset (__errnostr, '\0', ERR_WRAPPER_STR_MAX_LEN);                      \
+  strerror_r(__save_errno, __errnostr, ERR_WRAPPER_STR_MAX_LEN);           \
+  fprintf (stderr,                                                         \
+           "%s: %d: %s: errno '%s' (%d)\n",                                \
+           __FILE__, __LINE__, __PRETTY_FUNCTION__,                        \
+           __errnostr, __save_errno);                                      \
+  fflush (stderr);                                                         \
+  __errno_orig = __save_errno;                                             \
 } while (0)
 #else
 #define __MSG_TRACE(__msgtracestr, __msgtracenum)
 #define __ERRNO_TRACE(__errno_orig)
 #endif /* IPMI_TRACE */
 
-#define ERR_TRACE(__str, __num)                                         \
-do {                                                                    \
-  __MSG_TRACE(__str, __num);                                            \
+#define ERR_TRACE(__str, __num)                                            \
+do {                                                                       \
+  __MSG_TRACE(__str, __num);                                               \
 } while (0)
 
-#define SET_ERRNO(__errno)                                              \
-do {                                                                    \
-  errno = (__errno);                                                    \
-  __ERRNO_TRACE(errno);                                                 \
+#define SET_ERRNO(__errno)                                                 \
+do {                                                                       \
+  errno = (__errno);                                                       \
+  __ERRNO_TRACE(errno);                                                    \
 } while (0)
 
-#define ERRNO_TRACE(__errno)                                            \
-do {                                                                    \
-  __ERRNO_TRACE(__errno);                                               \
+#define ERRNO_TRACE(__errno)                                               \
+do {                                                                       \
+  __ERRNO_TRACE(__errno);                                                  \
 } while (0)
+
+#define FIID_OBJECT_ERROR_TO_ERRNO(__obj)                                  \
+do {                                                                       \
+  set_errno_by_fiid_object((__obj));                                       \
+  __MSG_TRACE(fiid_obj_errormsg((__obj)), fiid_obj_errnum((__obj)));       \
+} while (0)   
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* ipmi-err-wrappers.h */
+#endif /* ipmi-trace.h */
 
