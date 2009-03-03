48b84e11a2bdbe23edd1aa8b530dd733a57df5b9
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-03-03 00:38:00 +0000

    cleanup code

--- bmc-info/src/bmc-info-argp.c
+++ bmc-info/src/bmc-info-argp.c
@@ -1,19 +1,19 @@
-/* 
-   Copyright (C) 2003-2009 FreeIPMI Core Team
-   
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.  
+/*
+  Copyright (C) 2003-2009 FreeIPMI Core Team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
 */
 
 #if HAVE_CONFIG_H
@@ -38,7 +38,7 @@
 #include "tool-cmdline-common.h"
 #include "tool-config-file-common.h"
 
-const char *argp_program_version = 
+const char *argp_program_version =
   "bmc-info - " PACKAGE_VERSION "\n"
   "Copyright (C) 2003-2009 FreeIPMI Core Team\n"
   "This program is free software; you may redistribute it under the terms of\n"
@@ -47,12 +47,12 @@ const char *argp_program_version =
 const char *argp_program_bug_address =
   "<" PACKAGE_BUGREPORT ">";
 
-static char cmdline_doc[] = 
+static char cmdline_doc[] =
   "bmc-info - display BMC information";
 
 static char cmdline_args_doc[] = "";
 
-static struct argp_option cmdline_options[] = 
+static struct argp_option cmdline_options[] =
   {
     ARGP_COMMON_OPTIONS_DRIVER,
     ARGP_COMMON_OPTIONS_INBAND,
@@ -64,16 +64,16 @@ static struct argp_option cmdline_options[] =
     ARGP_COMMON_OPTIONS_WORKAROUND_FLAGS,
     ARGP_COMMON_HOSTRANGED_OPTIONS,
     ARGP_COMMON_OPTIONS_DEBUG,
-    {"guid", CMD_GUID_KEY, NULL, 0,
-     "Display device guid.", 30},
+    { "guid", CMD_GUID_KEY, NULL, 0,
+      "Display device guid.", 30},
     { 0 }
   };
 
 static error_t cmdline_parse (int key, char *arg, struct argp_state *state);
 
 static struct argp cmdline_argp = { cmdline_options,
-                                    cmdline_parse, 
-                                    cmdline_args_doc, 
+                                    cmdline_parse,
+                                    cmdline_args_doc,
                                     cmdline_doc };
 
 static struct argp cmdline_config_file_argp = { cmdline_options,
@@ -81,7 +81,7 @@ static struct argp cmdline_config_file_argp = { cmdline_options,
                                                 cmdline_args_doc,
                                                 cmdline_doc };
 
-static error_t 
+static error_t
 cmdline_parse (int key, char *arg, struct argp_state *state)
 {
   struct bmc_info_arguments *cmd_args = state->input;
@@ -104,12 +104,12 @@ cmdline_parse (int key, char *arg, struct argp_state *state)
         ret = hostrange_parse_opt (key, arg, state, &(cmd_args->hostrange));
       return ret;
     }
-  
+
   return 0;
 }
 
 static void
-_bmc_info_config_file_parse(struct bmc_info_arguments *cmd_args)
+_bmc_info_config_file_parse (struct bmc_info_arguments *cmd_args)
 {
   if (config_file_parse (cmd_args->common.config_file,
                          0,
@@ -120,12 +120,12 @@ _bmc_info_config_file_parse(struct bmc_info_arguments *cmd_args)
                          CONFIG_FILE_TOOL_BMC_INFO,
                          NULL) < 0)
     {
-      fprintf(stderr, "config_file_parse: %s\n", strerror(errno));
-      exit(1);
+      fprintf (stderr, "config_file_parse: %s\n", strerror (errno));
+      exit (1);
     }
 }
 
-void 
+void
 bmc_info_argp_parse (int argc, char **argv, struct bmc_info_arguments *cmd_args)
 {
   init_common_cmd_args_user (&(cmd_args->common));
@@ -133,19 +133,19 @@ bmc_info_argp_parse (int argc, char **argv, struct bmc_info_arguments *cmd_args)
 
   cmd_args->guid = 0;
 
-  argp_parse (&cmdline_config_file_argp, 
-              argc, 
-              argv, 
-              ARGP_IN_ORDER, NULL, 
+  argp_parse (&cmdline_config_file_argp,
+              argc,
+              argv,
+              ARGP_IN_ORDER, NULL,
               &(cmd_args->common));
 
-  _bmc_info_config_file_parse(cmd_args);
+  _bmc_info_config_file_parse (cmd_args);
 
-  argp_parse (&cmdline_argp, 
-              argc, 
-              argv, 
-              ARGP_IN_ORDER, 
-              NULL, 
+  argp_parse (&cmdline_argp,
+              argc,
+              argv,
+              ARGP_IN_ORDER,
+              NULL,
               cmd_args);
 
   verify_common_cmd_args (&(cmd_args->common));
