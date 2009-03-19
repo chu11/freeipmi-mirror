6495374c6fa3032752c9ef702676f6fdcd3b001c
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-03-19 18:01:24 +0000

    Support inventec get-nic-status and set-nic-status OEM commands.

--- ipmi-oem/src/ipmi-oem.c
+++ ipmi-oem/src/ipmi-oem.c
@@ -1,16 +1,16 @@
 /*
-  Copyright (C) 2008 FreeIPMI Core Team
+  Copyright (C) 2008-2009 FreeIPMI Core Team
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2, or (at your option)
   any later version.
-  
+
   This program is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA
@@ -57,20 +57,34 @@ struct ipmi_oem_id
   struct ipmi_oem_command *oem_commands;
 };
 
+static int _inventec_get_nic_status (ipmi_oem_state_data_t *);
+static int _inventec_set_nic_status (ipmi_oem_state_data_t *);
 static int _supermicro_reset_intrusion (ipmi_oem_state_data_t *);
+
+struct ipmi_oem_command oem_inventec[] =
+  {
+    { "get-nic-status",
+      "get NIC status.",
+      _inventec_get_nic_status},
+    { "set-nic-status",
+      "set NIC status.",
+      _inventec_set_nic_status},
+    { NULL, NULL, NULL},
+  };
 
 struct ipmi_oem_command oem_supermicro[] =
   {
-    {"reset-intrusion",
-     "reset motherboard intrusion flag.",
-     _supermicro_reset_intrusion},
-    {NULL, NULL, NULL},
+    { "reset-intrusion",
+      "reset motherboard intrusion flag.",
+      _supermicro_reset_intrusion},
+    { NULL, NULL, NULL},
   };
 
 struct ipmi_oem_id oem_cb[] =
   {
-    {"supermicro", oem_supermicro},
-    {NULL, NULL},
+    { "inventec", oem_inventec},
+    { "supermicro", oem_supermicro},
+    { NULL, NULL},
   };
 
 static int
@@ -80,23 +94,323 @@ _list (void)
 
   while (oem_id && oem_id->oem_id)
     {
-      struct ipmi_oem_command *oem_cmd = oem_cb->oem_commands;
+      struct ipmi_oem_command *oem_cmd = oem_id->oem_commands;
 
-      printf("OEM ID: %s\n", oem_id->oem_id);
+      printf ("OEM ID: %s\n", oem_id->oem_id);
 
       while (oem_cmd && oem_cmd->oem_command)
         {
-          printf("    Command: %s - %s\n", 
-                 oem_cmd->oem_command,
-                 oem_cmd->description);
+          printf ("    Command: %s - %s\n",
+                  oem_cmd->oem_command,
+                  oem_cmd->description);
           oem_cmd++;
         }
 
-      printf("\n");
+      printf ("\n");
       oem_id++;
     }
 
-  return 0;
+  return (0);
+}
+
+static int
+_check_response_and_completion_code (ipmi_oem_state_data_t *state_data,
+                                     uint8_t *bytes_rs,
+                                     unsigned int bytes_rs_len,
+                                     unsigned int expected_bytes_rs_len,
+                                     uint8_t cmd,
+                                     uint8_t netfn)
+{
+  assert (state_data);
+  assert (bytes_rs);
+  assert (expected_bytes_rs_len >= 2); /* need atleast cmd and completion code */
+
+  if (bytes_rs_len < expected_bytes_rs_len)
+    {
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "%s:%s invalid response length: %u, expected %u\n",
+                       state_data->prog_data->args->oem_id,
+                       state_data->prog_data->args->oem_command,
+                       bytes_rs_len,
+                       expected_bytes_rs_len);
+      return (-1);
+    }
+  
+  if (bytes_rs[1] != IPMI_COMP_CODE_COMMAND_SUCCESS)
+    {
+      char errbuf[IPMI_OEM_ERR_BUFLEN];
+      
+      memset (errbuf, '\0', IPMI_OEM_ERR_BUFLEN);
+      if (ipmi_completion_code_strerror_r (cmd, /* cmd */
+                                           netfn, /* network function */
+                                           bytes_rs[1], /* completion code */
+                                           errbuf,
+                                           IPMI_OEM_ERR_BUFLEN) < 0)
+        {
+          pstdout_perror (state_data->pstate, "ipmi_completion_code_strerror_r");
+          snprintf (errbuf, IPMI_OEM_ERR_BUFLEN, "completion-code = 0x%X", bytes_rs[1]);
+        }
+      
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "%s:%s failed: %s\n",
+                       state_data->prog_data->args->oem_id,
+                       state_data->prog_data->args->oem_command,
+                       errbuf);
+      return (-1);
+    }
+
+  return (0);
+}
+
+static int
+_inventec_get_reservation (ipmi_oem_state_data_t *state_data,
+                           uint8_t *reservation_id)
+{
+  uint8_t bytes_rq[IPMI_OEM_MAX_BYTES];
+  uint8_t bytes_rs[IPMI_OEM_MAX_BYTES];
+  int32_t rs_len;
+  int rv = -1;
+
+  /* Inventec OEM
+   *
+   * Get Reservation Request
+   *
+   * 0x30 - OEM network function
+   * 0x01 - OEM cmd
+   *
+   * Get Reservation Response 
+   *
+   * 0x01 - OEM cmd
+   * 0x?? - Completion Code
+   * 0x?? - Reservation ID
+   */
+
+  assert (state_data);
+  assert (reservation_id);
+
+  bytes_rq[0] = 0x01;
+
+  if ((rs_len = ipmi_cmd_raw (state_data->ipmi_ctx,
+                              0, /* lun */
+                              0x30, /* network function */
+                              bytes_rq, /* data */
+                              1, /* num bytes */
+                              bytes_rs,
+                              IPMI_OEM_MAX_BYTES)) < 0)
+    {
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "ipmi_cmd_raw: %s\n",
+                       ipmi_ctx_strerror (ipmi_ctx_errnum (state_data->ipmi_ctx)));
+      goto cleanup;
+    }
+
+  if (_check_response_and_completion_code (state_data,
+                                           bytes_rs,
+                                           rs_len,
+                                           3,
+                                           0x01,
+                                           0x30) < 0)
+    goto cleanup;
+  
+  (*reservation_id) = bytes_rs[2];
+
+  rv = 0;
+ cleanup:
+  return (rv);
+}
+
+static int
+_inventec_get_nic_status (ipmi_oem_state_data_t *state_data)
+{
+  uint8_t bytes_rq[IPMI_OEM_MAX_BYTES];
+  uint8_t bytes_rs[IPMI_OEM_MAX_BYTES];
+  int32_t rs_len;
+  uint8_t reservation_id;
+  int rv = -1;
+
+  assert (state_data);
+
+  /* Inventec OEM
+   *
+   * Get NIC Status Request
+   *
+   * 0x30 - OEM network function
+   * 0x02 - OEM cmd
+   * 0x?? - Reservation ID
+   * 0x02 - ??
+   * 0x01 - ??
+   * 0x00 - ??
+   * 0x00 - ??
+   * 0x00 = ??
+   * 0xFF - ??
+   * 
+   * Get NIC Status Response
+   *
+   * 0x02 - OEM cmd
+   * 0x?? - Completion Code
+   * 0x02 - ??
+   * 0x01 - ??
+   * 0x00 - ??
+   * 0x00 - ??
+   * 0x00 | 0x01 - 0x00 = shared, 0x01 = dedicated
+   */
+
+  if (_inventec_get_reservation (state_data,
+                                 &reservation_id) < 0)
+    goto cleanup;
+
+  bytes_rq[0] = 0x02;
+  bytes_rq[1] = reservation_id;
+  bytes_rq[2] = 0x02;
+  bytes_rq[3] = 0x01;
+  bytes_rq[4] = 0x00;
+  bytes_rq[5] = 0x00;
+  bytes_rq[6] = 0x00;
+  bytes_rq[7] = 0xFF;
+
+  if ((rs_len = ipmi_cmd_raw (state_data->ipmi_ctx,
+                              0, /* lun */
+                              0x30, /* network function */
+                              bytes_rq, /* data */
+                              8, /* num bytes */
+                              bytes_rs,
+                              IPMI_OEM_MAX_BYTES)) < 0)
+    {
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "ipmi_cmd_raw: %s\n",
+                       ipmi_ctx_strerror (ipmi_ctx_errnum (state_data->ipmi_ctx)));
+      goto cleanup;
+    }
+
+  if (_check_response_and_completion_code (state_data,
+                                           bytes_rs,
+                                           rs_len,
+                                           7,
+                                           0x02,
+                                           0x30) < 0)
+    goto cleanup;
+
+  switch (bytes_rs[6])
+    {
+    case 0:
+      pstdout_printf (state_data->pstate, "shared\n");
+      break;
+    case 1:
+      pstdout_printf (state_data->pstate, "dedicated\n");
+      break;
+    default:
+      pstdout_printf (state_data->pstate, "unknown NIC status: %Xh\n", bytes_rs[7]);
+      break;
+    }
+
+  rv = 0;
+ cleanup:
+  return (rv);
+}
+
+static int
+_inventec_set_nic_status (ipmi_oem_state_data_t *state_data)
+{
+  uint8_t bytes_rq[IPMI_OEM_MAX_BYTES];
+  uint8_t bytes_rs[IPMI_OEM_MAX_BYTES];
+  int32_t rs_len;
+  uint8_t reservation_id;
+  int rv = -1;
+
+  assert (state_data);
+
+  if (state_data->prog_data->args->oem_options_count != 1)
+    {
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "%s:%s invalid number of OEM option arguments\n",
+                       state_data->prog_data->args->oem_id,
+                       state_data->prog_data->args->oem_command);
+      goto cleanup;
+    }
+
+  if (strcasecmp (state_data->prog_data->args->oem_options[0], "shared")
+      && strcasecmp (state_data->prog_data->args->oem_options[0], "dedicated"))
+    {
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "%s:%s invalid OEM option argument '%s'\n",
+                       state_data->prog_data->args->oem_id,
+                       state_data->prog_data->args->oem_command,
+                       state_data->prog_data->args->oem_options[0]);
+      goto cleanup;
+    }
+
+  /* Inventec OEM
+   *
+   * Set NIC Status Request
+   *
+   * 0x30 - OEM network function
+   * 0x03 - OEM cmd
+   * 0x?? - Reservation ID
+   * 0x02 - ??
+   * 0x01 - ??
+   * 0x00 - ??
+   * 0x00 - ??
+   * 0x00 = ??
+   * 0x01 - ??
+   * 0x00 | 0x01 - 0x00 = shared, 0x01 = dedicated
+   * 
+   * Set NIC Status Response
+   *
+   * 0x03 - OEM cmd
+   * 0x?? - Completion Code
+   * 0x?? - ??
+   */
+
+  if (_inventec_get_reservation (state_data,
+                                 &reservation_id) < 0)
+    goto cleanup;
+
+  bytes_rq[0] = 0x03;
+  bytes_rq[1] = reservation_id;
+  bytes_rq[2] = 0x02;
+  bytes_rq[3] = 0x01;
+  bytes_rq[4] = 0x00;
+  bytes_rq[5] = 0x00;
+  bytes_rq[6] = 0x00;
+  bytes_rq[7] = 0x01;
+
+  if (!strcasecmp (state_data->prog_data->args->oem_options[0], "shared"))
+    bytes_rq[8] = 0x00;
+  else
+    bytes_rq[8] = 0x01;
+
+  if ((rs_len = ipmi_cmd_raw (state_data->ipmi_ctx,
+                              0, /* lun */
+                              0x30, /* network function */
+                              bytes_rq, /* data */
+                              9, /* num bytes */
+                              bytes_rs,
+                              IPMI_OEM_MAX_BYTES)) < 0)
+    {
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "ipmi_cmd_raw: %s\n",
+                       ipmi_ctx_strerror (ipmi_ctx_errnum (state_data->ipmi_ctx)));
+      goto cleanup;
+    }
+
+  if (_check_response_and_completion_code (state_data,
+                                           bytes_rs,
+                                           rs_len,
+                                           2, /* don't care about the 3rd byte, don't know what it is used for */
+                                           0x03,
+                                           0x30) < 0)
+    goto cleanup;
+
+  rv = 0;
+ cleanup:
+  return (rv);
 }
 
 static int
@@ -106,10 +420,10 @@ _supermicro_reset_intrusion (ipmi_oem_state_data_t *state_data)
   uint8_t bytes_rs[IPMI_OEM_MAX_BYTES];
   int32_t rs_len;
   int rv = -1;
-  
-  assert(state_data);
-  
-  /* Supermicro OEM 
+
+  assert (state_data);
+
+  /* Supermicro OEM
    *
    * 0x30 - OEM network function
    * 0x03 - OEM cmd
@@ -125,44 +439,21 @@ _supermicro_reset_intrusion (ipmi_oem_state_data_t *state_data)
                               bytes_rs,
                               IPMI_OEM_MAX_BYTES)) < 0)
     {
-      pstdout_fprintf(state_data->pstate,
-                      stderr,
-                      "ipmi_cmd_raw: %s\n",
-                      ipmi_ctx_strerror(ipmi_ctx_errnum(state_data->ipmi_ctx)));
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "ipmi_cmd_raw: %s\n",
+                       ipmi_ctx_strerror (ipmi_ctx_errnum (state_data->ipmi_ctx)));
       goto cleanup;
     }
 
-  if (rs_len < 2)
-    {
-      pstdout_fprintf(state_data->pstate,
-                      stderr,
-                      "reset-intrusion invalid response length: %d\n",
-                      rs_len);
-      goto cleanup;
-    }
+  if (_check_response_and_completion_code (state_data,
+                                           bytes_rs,
+                                           rs_len,
+                                           2,
+                                           0x03,
+                                           0x30) < 0)
+    goto cleanup;
 
-  if (bytes_rs[1] != IPMI_COMP_CODE_COMMAND_SUCCESS)
-    {
-      char errbuf[IPMI_OEM_ERR_BUFLEN];
-
-      memset(errbuf, '\0', IPMI_OEM_ERR_BUFLEN);
-      if (ipmi_completion_code_strerror_r(0x3,
-                                          0x30,
-                                          bytes_rs[1],
-                                          errbuf,
-                                          IPMI_OEM_ERR_BUFLEN) < 0)
-        {
-          pstdout_perror(state_data->pstate, "ipmi_completion_code_strerror_r");
-          snprintf(errbuf, IPMI_OEM_ERR_BUFLEN, "completion-code = 0x%X", bytes_rs[1]);
-        }
-      
-      pstdout_fprintf(state_data->pstate,
-                      stderr,
-                      "reset-intrusion failed: %s\n",
-                      errbuf);
-      goto cleanup;
-    }
-  
   rv = 0;
  cleanup:
   return (rv);
@@ -176,28 +467,30 @@ _run_oem_cmd (ipmi_oem_state_data_t *state_data)
   int id_found = 0;
   int rv = -1;
 
-  assert(state_data);
+  assert (state_data);
 
   args = state_data->prog_data->args;
 
   while (oem_id && oem_id->oem_id)
     {
-      if (!strcasecmp(oem_id->oem_id, args->oem_id))
+      if (!strcasecmp (oem_id->oem_id, args->oem_id))
         {
-          struct ipmi_oem_command *oem_cmd = oem_cb->oem_commands;
+          struct ipmi_oem_command *oem_cmd = oem_id->oem_commands;
           int cmd_found = 0;
-          
+
           id_found++;
 
           while (oem_cmd && oem_cmd->oem_command)
             {
-              if (!strcasecmp(oem_cmd->oem_command, 
-                              args->oem_command))
+              if (!strcasecmp (oem_cmd->oem_command,
+                               args->oem_command))
                 {
                   cmd_found++;
 
                   if (((*oem_cmd->func)(state_data)) < 0)
                     goto cleanup;
+
+                  break;
                 }
 
               oem_cmd++;
@@ -211,6 +504,8 @@ _run_oem_cmd (ipmi_oem_state_data_t *state_data)
                                args->oem_command);
               goto cleanup;
             }
+
+          break;
         }
 
       oem_id++;
@@ -236,14 +531,14 @@ run_cmd_args (ipmi_oem_state_data_t *state_data)
   struct ipmi_oem_arguments *args;
   int rv = -1;
 
-  assert(state_data);
+  assert (state_data);
 
   args = state_data->prog_data->args;
 
   /* shouldn't be possible at this point, make sure we've already
-   * exitted 
+   * exitted
    */
-  assert(!args->list);
+  assert (!args->list);
 
   if (!args->oem_id)
     {
@@ -260,10 +555,10 @@ run_cmd_args (ipmi_oem_state_data_t *state_data)
                        "OEM Command not specified\n");
       goto cleanup;
     }
-  
+
   if (_run_oem_cmd (state_data) < 0)
     goto cleanup;
- 
+
   rv = 0;
  cleanup:
   return (rv);
@@ -280,21 +575,21 @@ _ipmi_oem (pstdout_state_t pstate,
   int exit_code = -1;
 
   prog_data = (ipmi_oem_prog_data_t *)arg;
-  memset(&state_data, '\0', sizeof(ipmi_oem_state_data_t));
+  memset (&state_data, '\0', sizeof (ipmi_oem_state_data_t));
 
   state_data.prog_data = prog_data;
   state_data.pstate = pstate;
 
-  if (!(state_data.ipmi_ctx = ipmi_open(prog_data->progname,
-                                        hostname,
-                                        &(prog_data->args->common),
-                                        errmsg,
-                                        IPMI_OPEN_ERRMSGLEN)))
+  if (!(state_data.ipmi_ctx = ipmi_open (prog_data->progname,
+                                         hostname,
+                                         &(prog_data->args->common),
+                                         errmsg,
+                                         IPMI_OPEN_ERRMSGLEN)))
     {
-      pstdout_fprintf(pstate,
-                      stderr,
-                      "%s\n",
-                      errmsg);
+      pstdout_fprintf (pstate,
+                       stderr,
+                       "%s\n",
+                       errmsg);
       exit_code = EXIT_FAILURE;
       goto cleanup;
     }
@@ -312,7 +607,7 @@ _ipmi_oem (pstdout_state_t pstate,
       ipmi_ctx_close (state_data.ipmi_ctx);
       ipmi_ctx_destroy (state_data.ipmi_ctx);
     }
-  return exit_code;
+  return (exit_code);
 }
 
 int
@@ -323,9 +618,9 @@ main (int argc, char **argv)
   int exit_code;
   int rv;
 
-  ipmi_disable_coredump();
+  ipmi_disable_coredump ();
 
-  memset(&prog_data, '\0', sizeof(ipmi_oem_prog_data_t));
+  memset (&prog_data, '\0', sizeof (ipmi_oem_prog_data_t));
   prog_data.progname = argv[0];
   ipmi_oem_argp_parse (argc, argv, &cmd_args);
   prog_data.args = &cmd_args;
@@ -333,7 +628,7 @@ main (int argc, char **argv)
   /* Special case, just output list, don't do anything else */
   if (cmd_args.list)
     {
-      if (_list() < 0)
+      if (_list () < 0)
         {
           exit_code = EXIT_FAILURE;
           goto cleanup;
@@ -342,24 +637,24 @@ main (int argc, char **argv)
       goto cleanup;
     }
 
-  if (pstdout_setup(&(prog_data.args->common.hostname),
-                    prog_data.args->hostrange.buffer_output,
-                    prog_data.args->hostrange.consolidate_output,
-                    prog_data.args->hostrange.fanout,
-                    prog_data.args->hostrange.eliminate,
-                    prog_data.args->hostrange.always_prefix) < 0)
+  if (pstdout_setup (&(prog_data.args->common.hostname),
+                     prog_data.args->hostrange.buffer_output,
+                     prog_data.args->hostrange.consolidate_output,
+                     prog_data.args->hostrange.fanout,
+                     prog_data.args->hostrange.eliminate,
+                     prog_data.args->hostrange.always_prefix) < 0)
     {
       exit_code = EXIT_FAILURE;
       goto cleanup;
     }
 
-  if ((rv = pstdout_launch(prog_data.args->common.hostname,
-                           _ipmi_oem,
-                           &prog_data)) < 0)
+  if ((rv = pstdout_launch (prog_data.args->common.hostname,
+                            _ipmi_oem,
+                            &prog_data)) < 0)
     {
-      fprintf(stderr,
-              "pstdout_launch: %s\n",
-              pstdout_strerror(pstdout_errnum));
+      fprintf (stderr,
+               "pstdout_launch: %s\n",
+               pstdout_strerror (pstdout_errnum));
       exit_code = EXIT_FAILURE;
       goto cleanup;
     }
