906aa0c6f01e2c76bc5a5c2dea9967a910da2a8a
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-03-26 21:44:06 +0000

    output both bytes of firmware minor revision

--- bmc-info/src/bmc-info.c
+++ bmc-info/src/bmc-info.c
@@ -180,7 +180,7 @@ display_get_device_id (bmc_info_state_data_t *state_data)
     _FIID_OBJ_GET (cmd_rs, "firmware_revision2.minor_revision", &min);
     /* achu: minor revision is BCD encoded and is > 4 bits, output w/ %x */
     pstdout_printf(state_data->pstate, 
-                   "Firmware Revision: %d.%x\n", 
+                   "Firmware Revision: %d.%02x\n", 
                    (unsigned int) maj, 
                    (unsigned int) min);
   }
