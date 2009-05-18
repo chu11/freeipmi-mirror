cf5a8943d811cbe6a418a5a18e0a1fe131d4bba7
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-05-18 22:50:20 +0000

    fix volatile vs. non-volatile bug

--- bmc-config/src/bmc-config-channel-common.c
+++ bmc-config/src/bmc-config-channel-common.c
@@ -94,7 +94,7 @@ _get_key_info(bmc_config_state_data_t *state_data,
     return ret;
 
   /* Must check for Non_Volatile b/c Volatile is a substring of the former */
-  if (stristr(section_name, "Non_Volatile"))
+  if (stristr(key_name, "Non_Volatile"))
     *access_type = IPMI_CHANNEL_ACCESS_GET_NON_VOLATILE;
   else
     *access_type = IPMI_CHANNEL_ACCESS_GET_VOLATILE;
@@ -123,7 +123,7 @@ _set_key_info(bmc_config_state_data_t *state_data,
     return ret;
 
   /* Must check for Non_Volatile b/c Volatile is a substring of the former */
-  if (stristr(section_name, "Non_Volatile"))
+  if (stristr(key_name, "Non_Volatile"))
     *access_type = IPMI_CHANNEL_ACCESS_SET_NON_VOLATILE;
   else
     *access_type = IPMI_CHANNEL_ACCESS_SET_VOLATILE;
