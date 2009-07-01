ddb01a5d73fabc4f262edc0b5d15eba7bed7f35a
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-07-01 16:29:48 +0000

    make IPMI_COMP_CODE_REQUEST_PARAMETER_NOT_SUPPORTED an ok sensor error

--- libfreeipmi/src/sensor-read/ipmi-sensor-read.c
+++ libfreeipmi/src/sensor-read/ipmi-sensor-read.c
@@ -221,7 +221,9 @@ _sensor_reading_corner_case_checks (ipmi_sensor_read_ctx_t ctx,
            || (ipmi_check_completion_code (obj_cmd_rs,
                                            IPMI_COMP_CODE_REQUEST_INVALID_DATA_FIELD) == 1)
            || (ipmi_check_completion_code (obj_cmd_rs, 
-                                           IPMI_COMP_CODE_COMMAND_CANNOT_RESPOND) == 1))
+                                           IPMI_COMP_CODE_COMMAND_CANNOT_RESPOND) == 1)
+           || (ipmi_check_completion_code (obj_cmd_rs,
+                                           IPMI_COMP_CODE_REQUEST_PARAMETER_NOT_SUPPORTED) == 1))
     {
       /* A sensor listed by the SDR is not present or cannot be obtained for some reason */
       SENSOR_READ_SET_ERRNUM (ctx, IPMI_SENSOR_READ_ERR_SENSOR_READING_CANNOT_BE_OBTAINED);
