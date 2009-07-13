8d9c0e799e2b43f9b8b7b95628a13fde6214f04b
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-07-13 17:18:25 +0000

    add detail on oem hex codes

--- ipmi-oem/src/ipmi-oem-inventec.c
+++ ipmi-oem/src/ipmi-oem-inventec.c
@@ -118,21 +118,21 @@ ipmi_oem_inventec_get_nic_status (ipmi_oem_state_data_t *state_data)
    * 0x30 - OEM network function
    * 0x02 - OEM cmd
    * 0x?? - Reservation ID
-   * 0x02 - ??
-   * 0x01 - ??
-   * 0x00 - ??
-   * 0x00 - ??
-   * 0x00 = ??
-   * 0xFF - ??
+   * 0x02 - Configuration ID (0x02 = ??)
+   * 0x01 - Attribute ID (0x01 = ??)
+   * 0x00 - Index (unused here??)
+   * 0x00 - Data Offset - LSB (unused here??)
+   * 0x00 = Data Offset - MSB (unused here??)
+   * 0xFF - Bytes to read (0xFF = all)
    * 
    * Get NIC Status Response
    *
    * 0x02 - OEM cmd
    * 0x?? - Completion Code
-   * 0x02 - ??
-   * 0x01 - ??
-   * 0x00 - ??
-   * 0x00 - ??
+   * 0x02 - Configuration ID (0x02 = ??)
+   * 0x01 - Attribute ID (0x01 = ??)
+   * 0x00 - Index (unused here??)
+   * 0x01 - number of bytes returned
    * 0x00 | 0x01 - 0x00 = shared, 0x01 = dedicated
    */
 
@@ -221,19 +221,19 @@ ipmi_oem_inventec_set_nic_status (ipmi_oem_state_data_t *state_data)
    * 0x30 - OEM network function
    * 0x03 - OEM cmd
    * 0x?? - Reservation ID
-   * 0x02 - ??
-   * 0x01 - ??
-   * 0x00 - ??
-   * 0x00 - ??
-   * 0x00 = ??
-   * 0x01 - ??
+   * 0x02 - Configuration ID (0x02 = ??)
+   * 0x01 - Attribute ID (0x01 = ??)
+   * 0x00 - Index (unused here??)
+   * 0x00 - Data Offset - LSB (unused here??)
+   * 0x00 = Data Offset - MSB (unused here??)
+   * 0x01 - In progress bit (0x00 in progress, 0x01 - last config in this request)
    * 0x00 | 0x01 - 0x00 = shared, 0x01 = dedicated
    * 
    * Set NIC Status Response
    *
    * 0x03 - OEM cmd
    * 0x?? - Completion Code
-   * 0x?? - ??
+   * 0x?? - bytes written
    */
 
   if (_inventec_get_reservation (state_data,
