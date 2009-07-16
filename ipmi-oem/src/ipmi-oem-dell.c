8f331cef9a92761a2e4665bec9f406f65fd7f4fb
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-07-16 18:56:32 +0000

    update for newer freeipmi

--- ipmi-oem/src/ipmi-oem-dell.c
+++ ipmi-oem/src/ipmi-oem-dell.c
@@ -479,7 +479,7 @@ ipmi_oem_dell_get_fcb_version (ipmi_oem_state_data_t *state_data)
       pstdout_fprintf (state_data->pstate,
                        stderr,
                        "ipmi_cmd_raw: %s\n",
-                       ipmi_ctx_strerror (ipmi_ctx_errnum (state_data->ipmi_ctx)));
+                       ipmi_ctx_errormsg (state_data->ipmi_ctx));
       goto cleanup;
     }
 
