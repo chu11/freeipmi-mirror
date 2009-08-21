fb36b10afc03a9bbc57ebc65bdab18b01fbd887d
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-08-21 23:00:39 +0000

    Report some fatal errors under non-veborse mode.

--- ipmi-pef-config/src/ipmi-pef-config-sections.c
+++ ipmi-pef-config/src/ipmi-pef-config-sections.c
@@ -56,40 +56,36 @@ ipmi_pef_config_sections_create (ipmi_pef_config_state_data_t *state_data)
   if (get_number_of_lan_alert_destinations (state_data,
                                             &number_of_lan_alert_destinations) != CONFIG_ERR_SUCCESS)
     {
-      if (state_data->prog_data->args->config_args.verbose_count)
-        pstdout_fprintf (state_data->pstate,
-                         stderr,
-                         "## FATAL: Unable to get Number of Alert Destinations\n");
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "Unable to get Number of Alert Destinations\n");
       return (NULL);
     }
 
   if (get_number_of_alert_policy_entries (state_data,
                                           &number_of_alert_policy_entries) != CONFIG_ERR_SUCCESS)
     {
-      if (state_data->prog_data->args->config_args.verbose_count)
-        pstdout_fprintf (state_data->pstate,
-                         stderr,
-                         "## FATAL: Unable to get Number of Alert Policy Entries\n");
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "Unable to get Number of Alert Policy Entries\n");
       return (NULL);
     }
 
   if (get_number_of_alert_strings (state_data,
                                    &number_of_alert_strings) != CONFIG_ERR_SUCCESS)
     {
-      if (state_data->prog_data->args->config_args.verbose_count)
-        pstdout_fprintf (state_data->pstate,
-                         stderr,
-                         "## FATAL: Unable to get Number of Alert Strings\n");
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "Unable to get Number of Alert Strings\n");
       return (NULL);
     }
 
   if (get_number_of_event_filters (state_data,
                                    &number_of_event_filters) != CONFIG_ERR_SUCCESS)
     {
-      if (state_data->prog_data->args->config_args.verbose_count)
-        pstdout_fprintf (state_data->pstate,
-                         stderr,
-                         "## FATAL: Unable to get Number of Event Filters\n");
+      pstdout_fprintf (state_data->pstate,
+                       stderr,
+                       "Unable to get Number of Event Filters\n");
       return (NULL);
     }
 
