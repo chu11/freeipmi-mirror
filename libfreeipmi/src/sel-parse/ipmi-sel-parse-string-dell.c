f4e90896647c2dc864f2c82d73444bd13fced6b1
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-11-17 22:35:51 +0000

    code cleanup

--- libfreeipmi/src/sel-parse/ipmi-sel-parse-string-dell.c
+++ libfreeipmi/src/sel-parse/ipmi-sel-parse-string-dell.c
@@ -477,23 +477,22 @@ ipmi_sel_parse_output_dell_event_data2_discrete_oem (ipmi_sel_parse_ctx_t ctx,
 
     }
 
-  /* achu: I don't know what motherboards this applies to */
+  /* achu: I don't know what motherboards this applies to, probably very old ones */
 #if 0
   /* OEM Interpretation
    *
    * From Dell Spec and Dell Code
    */
   if (ctx->manufacturer_id == IPMI_IANA_ENTERPRISE_ID_DELL
-      && ctx->ipmi_version_major == IPMI_1_5_MAJOR_VERSION
-      && ctx->ipmi_version_minor == IPMI_1_5_MINOR_VERSION
       && system_event_record_data->event_type_code == IPMI_EVENT_READING_TYPE_CODE_SENSOR_SPECIFIC
       && system_event_record_data->sensor_type == IPMI_SENSOR_TYPE_MEMORY
       && (system_event_record_data->offset_from_event_reading_type_code == IPMI_SENSOR_TYPE_MEMORY_PRESENCE_DETECTED
-          || system_event_record_data->offset_from_event_reading_type_code == IPMI_SENSOR_TYPE_MEMORY_CONFIGURATION_ERROR))
+          || system_event_record_data->offset_from_event_reading_type_code == IPMI_SENSOR_TYPE_MEMORY_CONFIGURATION_ERROR)
+      && ctx->ipmi_version_major == IPMI_1_5_MAJOR_VERSION
+      && ctx->ipmi_version_minor == IPMI_1_5_MINOR_VERSION)
     {
       uint8_t memory_card;
       uint8_t bank_number;
-      char memory_card_char;
           
       memory_card = (system_event_record_data->event_data2 & IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_MEMORY_CARD_BITMASK);
       memory_card >>= IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_MEMORY_CARD_SHIFT;
@@ -501,16 +500,13 @@ ipmi_sel_parse_output_dell_event_data2_discrete_oem (ipmi_sel_parse_ctx_t ctx,
       bank_number = (system_event_record_data->event_data2 & IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_BANK_NUMBER_BITMASK);
       bank_number >>= IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_BANK_NUMBER_SHIFT;
 
-      if (memory_card != IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_NO_CARD)
-        memory_card_char = 'A' + memory_card;
-
       if (IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_MEMORY_CARD_VALID (memory_card)
           && IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_BANK_NUMBER_VALID (bank_number))
         {
           snprintf (tmpbuf,
                     tmpbuflen,
                     "Memory Card %c, Bank %u",
-                    memory_card_char,
+                    'A' + memory_card,
                     bank_number);
           return (1);
         }
@@ -519,7 +515,7 @@ ipmi_sel_parse_output_dell_event_data2_discrete_oem (ipmi_sel_parse_ctx_t ctx,
           snprintf (tmpbuf,
                     tmpbuflen,
                     "Memory Card %c",
-                    memory_card_char);
+                    'A' + memory_card);
           return (1);
         }
       else if (IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_BANK_NUMBER_VALID (bank_number))
@@ -713,12 +709,12 @@ ipmi_sel_parse_output_dell_event_data3_discrete_oem (ipmi_sel_parse_ctx_t ctx,
    * From Dell Spec and Dell Code
    */
   if (ctx->manufacturer_id == IPMI_IANA_ENTERPRISE_ID_DELL
-      && ctx->ipmi_version_major == IPMI_1_5_MAJOR_VERSION
-      && ctx->ipmi_version_minor == IPMI_1_5_MINOR_VERSION
       && system_event_record_data->event_type_code == IPMI_EVENT_READING_TYPE_CODE_SENSOR_SPECIFIC
       && system_event_record_data->sensor_type == IPMI_SENSOR_TYPE_MEMORY
       && (system_event_record_data->offset_from_event_reading_type_code == IPMI_SENSOR_TYPE_MEMORY_PRESENCE_DETECTED
-          || system_event_record_data->offset_from_event_reading_type_code == IPMI_SENSOR_TYPE_MEMORY_CONFIGURATION_ERROR))
+          || system_event_record_data->offset_from_event_reading_type_code == IPMI_SENSOR_TYPE_MEMORY_CONFIGURATION_ERROR)
+      && ctx->ipmi_version_major == IPMI_1_5_MAJOR_VERSION
+      && ctx->ipmi_version_minor == IPMI_1_5_MINOR_VERSION)
     {
       if (IPMI_SENSOR_TYPE_MEMORY_EVENT_DATA2_OEM_DELL_DIMM_NUMBER_VALID (system_event_record_data->event_data3))
         {
@@ -1325,6 +1321,7 @@ ipmi_sel_parse_output_dell_event_data2_event_data3 (ipmi_sel_parse_ctx_t ctx,
 
   /* achu: I don't know what motherboards this applies to */
 #if 0
+
   /* OEM Interpretation
    *
    * From Dell Provided Source Code
@@ -1332,8 +1329,18 @@ ipmi_sel_parse_output_dell_event_data2_event_data3 (ipmi_sel_parse_ctx_t ctx,
    * Specifically for Version Change Sensors with an event offset
    * IPMI_SENSOR_TYPE_VERSION_CHANGE_FIRMWARE_OR_SOFTWARE_INCOMPATABILITY_DETECTED_WITH_ASSOCIATED_ENTITY
    *
-   * achu: XXX: dataX & 0x1F != 1F ???  Need to ask Dell.
+   * achu: XXX: dataX & 0x1F != 1F ???  The bitmasks below have never
+   * been verified by Dell.
    */
+
+#define IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_ENTITY_BITMASK 0xE0
+#define IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_ENTITY_SHIFT      5
+
+#define IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_BITMASK 0x1F
+#define IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_SHIFT      0
+
+#define IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_INVALID 0x1F
+
   if (ctx->manufacturer_id == IPMI_IANA_ENTERPRISE_ID_DELL
       && ctx->ipmi_version_major == IPMI_2_0_MAJOR_VERSION
       && ctx->ipmi_version_minor == IPMI_2_0_MINOR_VERSION
@@ -1350,10 +1357,17 @@ ipmi_sel_parse_output_dell_event_data2_event_data3 (ipmi_sel_parse_ctx_t ctx,
       char data2_number_str[DELL_EVENT_BUFFER_LENGTH];
       char data3_number_str[DELL_EVENT_BUFFER_LENGTH];
 
-      data2_entity = system_event_record_data->event_data2 >> 3;
-      data2_number = system_event_record_data->event_data2 & 0x1F;
-      data3_entity = system_event_record_data->event_data3 >> 3;
-      data3_number = system_event_record_data->event_data3 & 0x1F;
+      data2_entity = (system_event_record_data->event_data2 & IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_ENTITY_BITMASK);
+      data2_entity >>= IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_ENTITY_SHIFT;
+
+      data2_number = (system_event_record_data->event_data2 & IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_BITMASK);
+      data2_number >>= IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_SHIFT;
+
+      data3_entity = (system_event_record_data->event_data3 & IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_ENTITY_BITMASK);
+      data3_entity >>= IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_ENTITY_SHIFT;
+
+      data3_number = (system_event_record_data->event_data3 & IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_BITMASK);
+      data3_number >>= IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_SHIFT;
 
       data2_entity_str = _dell_version_change_entity_string (data2_entity);
       data3_entity_str = _dell_version_change_entity_string (data3_entity);
@@ -1361,28 +1375,28 @@ ipmi_sel_parse_output_dell_event_data2_event_data3 (ipmi_sel_parse_ctx_t ctx,
       memset (data2_number_str, '\0', DELL_EVENT_BUFFER_LENGTH);
       memset (data3_number_str, '\0', DELL_EVENT_BUFFER_LENGTH);
 
-      if (data2_number != 0x1F)
+      if (data2_number != IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_INVALID)
         snprintf (data2_number_str,
                   DELL_EVENT_BUFFER_LENGTH,
                   "%u",
                   data2_number);
 
-      if (data3_number != 0x1F)
+      if (data3_number != IPMI_SENSOR_TYPE_MEMORY_OEM_DELL_VERSION_CHANGE_NUMBER_INVALID)
         snprintf (data3_number_str,
                   DELL_EVENT_BUFFER_LENGTH,
                   "%u",
                   data3_number);
 
       if (ipmi_sel_parse_string_snprintf (buf,
-					  buflen,
-					  wlen,
-					  "%s%s%s with %s%s%s",
-					  data2_entity_str,
-					  strlen (data2_number_str) ? " " : "",
-					  data2_number_str,
-					  data3_entity_str,
-					  strlen (data3_number_str) ? " " : "",
-					  data3_number_str))
+                                          buflen,
+                                          wlen,
+                                          "%s%s%s with %s%s%s",
+                                          data2_entity_str,
+                                          strlen (data2_number_str) ? " " : "",
+                                          data2_number_str,
+                                          data3_entity_str,
+                                          strlen (data3_number_str) ? " " : "",
+                                          data3_number_str))
         (*oem_rv) = 1;
       else
         (*oem_rv) = 0;
