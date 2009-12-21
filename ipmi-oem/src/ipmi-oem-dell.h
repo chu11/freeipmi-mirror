57d968bb66697b6e3669196cd196794de38fc1c5
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2009-12-21 18:59:08 +0000

    update get/reset-power-consumption-data for dell

--- ipmi-oem/src/ipmi-oem-dell.h
+++ ipmi-oem/src/ipmi-oem-dell.h
@@ -40,9 +40,9 @@ int ipmi_oem_dell_set_active_directory_config (ipmi_oem_state_data_t *state_data
 
 int ipmi_oem_dell_reset_to_defaults (ipmi_oem_state_data_t *state_data);
 
-int ipmi_oem_dell_get_power_info (ipmi_oem_state_data_t *state_data);
+int ipmi_oem_dell_get_power_consumption_data (ipmi_oem_state_data_t *state_data);
 
-int ipmi_oem_dell_reset_power_info (ipmi_oem_state_data_t *state_data);
+int ipmi_oem_dell_reset_power_consumption_data (ipmi_oem_state_data_t *state_data);
 
 int ipmi_oem_dell_get_power_supply_info (ipmi_oem_state_data_t *state_data);
 
