bf805ba855e7ae92dd757f6a45234cd338e042a9
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2010-06-11 16:37:48 +0000

    Fix units output corner case

--- ipmimonitoring/src/libipmimonitoring/ipmi_monitoring_sensor_reading.c
+++ ipmimonitoring/src/libipmimonitoring/ipmi_monitoring_sensor_reading.c
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  $Id: ipmi_monitoring_sensor_reading.c,v 1.90 2010-03-19 22:07:58 chu11 Exp $
+ *  $Id: ipmi_monitoring_sensor_reading.c,v 1.91 2010-06-11 16:37:40 chu11 Exp $
  *****************************************************************************
  *  Copyright (C) 2007-2010 Lawrence Livermore National Security, LLC.
  *  Copyright (C) 2006-2007 The Regents of the University of California.
@@ -405,7 +405,7 @@ _get_sensor_units (ipmi_monitoring_ctx_t c,
   assert (c);
   assert (c->magic == IPMI_MONITORING_MAGIC);
 
-  if (sensor_units_modifier != IPMI_SDR_PERCENTAGE_NO)
+  if (sensor_units_percentage != IPMI_SDR_PERCENTAGE_NO)
     {
       IPMI_MONITORING_DEBUG (("sensor_units_percentage 'yes' not supported"));
       return (IPMI_MONITORING_SENSOR_UNITS_UNKNOWN);
