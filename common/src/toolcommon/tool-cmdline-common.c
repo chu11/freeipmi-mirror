fd36e686238ad003ac1fa663effe9a3298ca244c
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2010-06-16 17:53:04 +0000

    Support 'ignorestateflag' workaround.

--- common/src/toolcommon/tool-cmdline-common.c
+++ common/src/toolcommon/tool-cmdline-common.c
@@ -225,6 +225,9 @@ parse_workaround_flags (const char *str,
       else if (tool_specific_workaround_flags
                && !strcasecmp (tok, IPMI_TOOL_SPECIFIC_WORKAROUND_FLAGS_SLOW_COMMIT_STR))
         (*tool_specific_workaround_flags) |= IPMI_TOOL_SPECIFIC_WORKAROUND_FLAGS_SLOW_COMMIT;
+      else if (tool_specific_workaround_flags
+               && !strcasecmp (tok, IPMI_TOOL_SPECIFIC_WORKAROUND_FLAGS_IGNORE_STATE_FLAG_STR))
+        (*tool_specific_workaround_flags) |= IPMI_TOOL_SPECIFIC_WORKAROUND_FLAGS_IGNORE_STATE_FLAG;
       else
         return (-1);
       tok = strtok (NULL, ",");
