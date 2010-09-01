dc8b93065ea232720a1b7db357814c5a6ec461a5
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2010-09-01 23:32:33 +0000

    fix corner case

--- libfreeipmi/src/sel-parse/ipmi-sel-parse-string-intel.c
+++ libfreeipmi/src/sel-parse/ipmi-sel-parse-string-intel.c
@@ -535,7 +535,8 @@ ipmi_sel_parse_output_intel_event_data2_event_data3 (ipmi_sel_parse_ctx_t ctx,
   if (ctx->manufacturer_id == IPMI_IANA_ENTERPRISE_ID_INTEL
       && ctx->product_id == IPMI_INTEL_PRODUCT_ID_S5500WB)
     {
-      if (system_event_record_data->generator_id == IPMI_GENERATOR_ID_OEM_INTEL_BIOS_POST
+      if ((system_event_record_data->generator_id == IPMI_GENERATOR_ID_OEM_INTEL_BIOS_POST
+	   || system_event_record_data->generator_id == IPMI_GENERATOR_ID_OEM_INTEL_BIOS_SMI_HANDLER)
 	  && system_event_record_data->event_type_code == IPMI_EVENT_READING_TYPE_CODE_SENSOR_SPECIFIC
 	  && system_event_record_data->sensor_type == IPMI_SENSOR_TYPE_SYSTEM_FIRMWARE_PROGRESS
 	  && system_event_record_data->sensor_number == IPMI_SENSOR_NUMBER_OEM_INTEL_BIOS_POST_ERROR
