b78cb34c532477fde0fc8ee68e0c00eebebdb782
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2010-09-03 17:49:09 +0000

    fix typo

--- libfreeipmi/src/sel-parse/ipmi-sel-parse-string-quanta.c
+++ libfreeipmi/src/sel-parse/ipmi-sel-parse-string-quanta.c
@@ -89,7 +89,7 @@ ipmi_sel_parse_output_quanta_event_data2_discrete_oem (ipmi_sel_parse_ctx_t ctx,
 {
   assert (ctx);
   assert (ctx->magic == IPMI_SEL_PARSE_CTX_MAGIC);
-  assert (ctx->manufactuer_id == IPMI_IANA_ENTERPRISE_ID_QUANTA);
+  assert (ctx->manufacturer_id == IPMI_IANA_ENTERPRISE_ID_QUANTA);
   assert (sel_parse_entry);
   assert (tmpbuf);
   assert (tmpbuflen);
@@ -173,7 +173,7 @@ ipmi_sel_parse_output_quanta_event_data3_discrete_oem (ipmi_sel_parse_ctx_t ctx,
 {
   assert (ctx);
   assert (ctx->magic == IPMI_SEL_PARSE_CTX_MAGIC);
-  assert (ctx->manufactuer_id == IPMI_IANA_ENTERPRISE_ID_QUANTA);
+  assert (ctx->manufacturer_id == IPMI_IANA_ENTERPRISE_ID_QUANTA);
   assert (sel_parse_entry);
   assert (tmpbuf);
   assert (tmpbuflen);
