7e318eea9b6b2be249fefb063e9111fbf1c92264
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2010-10-22 21:11:20 +0000

    Fix corner case with --output-oem-event-strings, did not work with OEM record types.

--- ipmi-sel/src/ipmi-sel.c
+++ ipmi-sel/src/ipmi-sel.c
@@ -1080,7 +1080,7 @@ _normal_output_event (ipmi_sel_state_data_t *state_data, unsigned int flags)
 {
   char fmtbuf[IPMI_SEL_OUTPUT_BUFLEN+1];
   char outbuf[IPMI_SEL_OUTPUT_BUFLEN+1];
-  int outbuf_len;
+  int outbuf_len = 0;
   char *fmt;
   uint8_t event_type_code;
   uint8_t event_data2_flag;
@@ -1388,18 +1388,21 @@ _normal_output_oem_data (ipmi_sel_state_data_t *state_data, unsigned int flags)
         read_oem_event_string++;
     }
 
-  memset (outbuf, '\0', IPMI_SEL_OUTPUT_BUFLEN+1);
-  if ((outbuf_len = ipmi_sel_parse_read_record_string (state_data->sel_parse_ctx,
-                                                       "%o",
-                                                       outbuf,
-                                                       IPMI_SEL_OUTPUT_BUFLEN,
-                                                       flags)) < 0)
+  if (!read_oem_event_string)
     {
-      if (_sel_parse_err_handle (state_data, "ipmi_sel_parse_read_record_string") < 0)
-        return (-1);
-      return (0);
+      memset (outbuf, '\0', IPMI_SEL_OUTPUT_BUFLEN+1);
+      if ((outbuf_len = ipmi_sel_parse_read_record_string (state_data->sel_parse_ctx,
+                                                           "%o",
+                                                           outbuf,
+                                                           IPMI_SEL_OUTPUT_BUFLEN,
+                                                           flags)) < 0)
+        {
+          if (_sel_parse_err_handle (state_data, "ipmi_sel_parse_read_record_string") < 0)
+            return (-1);
+          return (0);
+        }
     }
-
+  
   if (state_data->prog_data->args->comma_separated_output)
     {
       if (outbuf_len)
