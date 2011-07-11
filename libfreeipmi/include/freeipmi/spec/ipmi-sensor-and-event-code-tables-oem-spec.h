90d44c40ad0d2a74764ad0dc81ac7a7cd7ce8be9
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2011-07-11 21:00:52 +0000

    Support Supermicro X8SIL-F OEM Sensors.

--- libfreeipmi/include/freeipmi/spec/ipmi-sensor-and-event-code-tables-oem-spec.h
+++ libfreeipmi/include/freeipmi/spec/ipmi-sensor-and-event-code-tables-oem-spec.h
@@ -836,7 +836,7 @@ extern unsigned int ipmi_oem_intel_specific_pci_correctable_sensor_max_index;
  *******************************************/
 
 /*
- * Supermicro X7DBR-3 (X7DBR_3), X8DTH, X8DTG, X8DTU, X8DT3-LN4F, X8DTU-6+ (X8DTU_6PLUS), X8DTL-3F (X8DTL_3F)
+ * Supermicro X7DBR-3 (X7DBR_3), X8DTH, X8DTG, X8DTU, X8DT3-LN4F, X8DTU-6+ (X8DTU_6PLUS), X8DTL-3F (X8DTL_3F), X8SIL-F (X8SIL_F)
  */
 /* achu: not official names, named based on use context */
 #define IPMI_SENSOR_TYPE_OEM_SUPERMICRO_CPU_TEMP_LOW           0x00
