2f8be2c11694e924809679b92ee87e8e09807daf
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2011-09-20 17:55:10 +0000

    support Support Supermicro X7DB8, X8DTN, X7SBI-LN4, X8DTL OEM sensors.

--- libfreeipmi/include/freeipmi/spec/ipmi-event-reading-type-code-oem-spec.h
+++ libfreeipmi/include/freeipmi/spec/ipmi-event-reading-type-code-oem-spec.h
@@ -92,11 +92,15 @@ extern "C" {
 
 /*
  * Supermicro X7DBR-3 (X7DBR_3)
+ * Supermicro X7DB8
+ * Supermicro X8DTN
+ * Supermicro X7SBI-LN4 (X7SBI_LN4)
  * Supermicro X8DTH
  * Supermicro X8DTG
  * Supermicro X8DTU
  * Supermicro X8DT3-LN4F (X8DT3_LN4F)
  * Supermicro X8DTU-6+ (X8DTU_6PLUS)
+ * Supermicro X8DTL
  * Supermicro X8DTL-3F (X8DTL_3F)
  * Supermicro X8SIL-F  (X8SIL_F)
  * Supermicro X9SCL
