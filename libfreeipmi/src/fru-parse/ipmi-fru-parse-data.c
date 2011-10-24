468d6f9afb46d39a404ba4ebe4b663ba44f8650b
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2011-10-24 20:20:50 +0000

    add forgotten headers

--- libfreeipmi/src/fru-parse/ipmi-fru-parse-data.c
+++ libfreeipmi/src/fru-parse/ipmi-fru-parse-data.c
@@ -54,6 +54,16 @@
 #if HAVE_UNISTD_H
 #include <unistd.h>
 #endif /* HAVE_UNISTD_H */
+#if TIME_WITH_SYS_TIME
+#include <sys/time.h>
+#include <time.h>
+#else  /* !TIME_WITH_SYS_TIME */
+#if HAVE_SYS_TIME_H
+#include <sys/time.h>
+#else /* !HAVE_SYS_TIME_H */
+#include <time.h>
+#endif  /* !HAVE_SYS_TIME_H */
+#endif /* !TIME_WITH_SYS_TIME */
 #include <assert.h>
 #include <errno.h>
 
