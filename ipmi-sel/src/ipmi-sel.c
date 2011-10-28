540a76441106143757e864c2cbc2558b123eba06
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2011-10-28 21:01:19 +0000

    Code cleanup, split function into _normal_output_sensor_name and _normal_output_sensor_type.  Update code accordingly.

--- ipmi-sel/src/ipmi-sel.c
+++ ipmi-sel/src/ipmi-sel.c
@@ -895,7 +895,7 @@ _normal_output_not_available_date_and_time (ipmi_sel_state_data_t *state_data)
  * return (-1) on error
  */
 static int
-_normal_output_sensor_name_and_type (ipmi_sel_state_data_t *state_data, unsigned int flags)
+_normal_output_sensor_name (ipmi_sel_state_data_t *state_data, unsigned int flags)
 {
   char fmt[IPMI_SEL_FMT_BUFLEN + 1];
   char outbuf[IPMI_SEL_OUTPUT_BUFLEN+1];
@@ -1020,40 +1020,6 @@ _normal_output_sensor_name_and_type (ipmi_sel_state_data_t *state_data, unsigned
   else
     pstdout_printf (state_data->pstate, fmt, IPMI_SEL_NA_STRING);
 
-  if (!state_data->prog_data->args->no_sensor_type_output)
-    {
-      memset (outbuf, '\0', IPMI_SEL_OUTPUT_BUFLEN+1);
-      if ((outbuf_len = ipmi_sel_parse_read_record_string (state_data->sel_parse_ctx,
-                                                           "%T",
-                                                           outbuf,
-                                                           IPMI_SEL_OUTPUT_BUFLEN,
-                                                           flags)) < 0)
-        {
-          if (_sel_parse_err_handle (state_data, "ipmi_sel_parse_read_record_string") < 0)
-            return (-1);
-          return (0);
-        }
-      
-      if (outbuf_len > state_data->column_width.sensor_type)
-        state_data->column_width.sensor_type = outbuf_len;
-      
-      memset (fmt, '\0', IPMI_SEL_FMT_BUFLEN + 1);
-      if (state_data->prog_data->args->comma_separated_output)
-        snprintf (fmt,
-                  IPMI_SEL_FMT_BUFLEN,
-                  ",%%s");
-      else
-        snprintf (fmt,
-                  IPMI_SEL_FMT_BUFLEN,
-                  " | %%-%ds",
-                  state_data->column_width.sensor_type);
-      
-      if (outbuf_len)
-        pstdout_printf (state_data->pstate, fmt, outbuf);
-      else
-        pstdout_printf (state_data->pstate, fmt, IPMI_SEL_NA_STRING);
-    }
-
   return (1);
 }
 
@@ -1062,7 +1028,7 @@ _normal_output_sensor_name_and_type (ipmi_sel_state_data_t *state_data, unsigned
  * return (-1) on error
  */
 static int
-_normal_output_not_available_sensor_name_and_type (ipmi_sel_state_data_t *state_data)
+_normal_output_not_available_sensor_name (ipmi_sel_state_data_t *state_data)
 {
   char fmt[IPMI_SEL_FMT_BUFLEN + 1];
 
@@ -1082,21 +1048,83 @@ _normal_output_not_available_sensor_name_and_type (ipmi_sel_state_data_t *state_
 
   pstdout_printf (state_data->pstate, fmt, IPMI_SEL_NA_STRING);
 
-  if (!state_data->prog_data->args->no_sensor_type_output)
+  return (1);
+}
+
+/* return 1 on success
+ * return (0) on non-success, but don't fail
+ * return (-1) on error
+ */
+static int
+_normal_output_sensor_type (ipmi_sel_state_data_t *state_data, unsigned int flags)
+{
+  char fmt[IPMI_SEL_FMT_BUFLEN + 1];
+  char outbuf[IPMI_SEL_OUTPUT_BUFLEN+1];
+  int outbuf_len;
+
+  assert (state_data);
+  assert (!state_data->prog_data->args->no_sensor_type_output);
+  assert (!state_data->prog_data->args->legacy_output);
+
+  memset (outbuf, '\0', IPMI_SEL_OUTPUT_BUFLEN+1);
+  if ((outbuf_len = ipmi_sel_parse_read_record_string (state_data->sel_parse_ctx,
+						       "%T",
+						       outbuf,
+						       IPMI_SEL_OUTPUT_BUFLEN,
+						       flags)) < 0)
     {
-      memset (fmt, '\0', IPMI_SEL_FMT_BUFLEN + 1);
-      if (state_data->prog_data->args->comma_separated_output)
-        snprintf (fmt,
-                  IPMI_SEL_FMT_BUFLEN,
-                  ",%%s");
-      else
-        snprintf (fmt,
-                  IPMI_SEL_FMT_BUFLEN,
-                  " | %%-%ds",
-                  state_data->column_width.sensor_type);
-      
-      pstdout_printf (state_data->pstate, fmt, IPMI_SEL_NA_STRING);
+      if (_sel_parse_err_handle (state_data, "ipmi_sel_parse_read_record_string") < 0)
+            return (-1);
+      return (0);
     }
+  
+  if (outbuf_len > state_data->column_width.sensor_type)
+    state_data->column_width.sensor_type = outbuf_len;
+  
+  memset (fmt, '\0', IPMI_SEL_FMT_BUFLEN + 1);
+  if (state_data->prog_data->args->comma_separated_output)
+    snprintf (fmt,
+	      IPMI_SEL_FMT_BUFLEN,
+	      ",%%s");
+  else
+    snprintf (fmt,
+	      IPMI_SEL_FMT_BUFLEN,
+	      " | %%-%ds",
+	      state_data->column_width.sensor_type);
+  
+  if (outbuf_len)
+    pstdout_printf (state_data->pstate, fmt, outbuf);
+  else
+    pstdout_printf (state_data->pstate, fmt, IPMI_SEL_NA_STRING);
+  
+  return (1);
+}
+
+/* return 1 on success
+ * return (0) on non-success, but don't fail
+ * return (-1) on error
+ */
+static int
+_normal_output_not_available_sensor_type (ipmi_sel_state_data_t *state_data)
+{
+  char fmt[IPMI_SEL_FMT_BUFLEN + 1];
+
+  assert (state_data);
+  assert (!state_data->prog_data->args->no_sensor_type_output);
+  assert (!state_data->prog_data->args->legacy_output);
+
+  memset (fmt, '\0', IPMI_SEL_FMT_BUFLEN + 1);
+  if (state_data->prog_data->args->comma_separated_output)
+    snprintf (fmt,
+	      IPMI_SEL_FMT_BUFLEN,
+	      ",%%s");
+  else
+    snprintf (fmt,
+	      IPMI_SEL_FMT_BUFLEN,
+	      " | %%-%ds",
+	      state_data->column_width.sensor_type);
+  
+  pstdout_printf (state_data->pstate, fmt, IPMI_SEL_NA_STRING);
 
   return (1);
 }
@@ -1692,11 +1720,20 @@ _normal_output (ipmi_sel_state_data_t *state_data, uint8_t record_type)
       if (!ret)
         goto newline_out;
 
-      if ((ret = _normal_output_sensor_name_and_type (state_data, flags)) < 0)
+      if ((ret = _normal_output_sensor_name (state_data, flags)) < 0)
         goto cleanup;
 
       if (!ret)
         goto newline_out;
+
+      if (!state_data->prog_data->args->no_sensor_type_output)
+	{
+	  if ((ret = _normal_output_sensor_type (state_data, flags)) < 0)
+	    goto cleanup;
+
+	  if (!ret)
+	    goto newline_out;
+	}
 
       if (state_data->prog_data->args->output_event_state)
         {
@@ -1736,11 +1773,20 @@ _normal_output (ipmi_sel_state_data_t *state_data, uint8_t record_type)
       if (!ret)
         goto newline_out;
 
-      if ((ret = _normal_output_not_available_sensor_name_and_type (state_data)) < 0)
+      if ((ret = _normal_output_not_available_sensor_name (state_data)) < 0)
         goto cleanup;
 
       if (!ret)
         goto newline_out;
+
+      if (!state_data->prog_data->args->no_sensor_type_output)
+	{
+	  if ((ret = _normal_output_not_available_sensor_type (state_data)) < 0)
+	    goto cleanup;
+
+	  if (!ret)
+	    goto newline_out;
+	}
 
       if (state_data->prog_data->args->output_event_state)
         {
@@ -1780,11 +1826,20 @@ _normal_output (ipmi_sel_state_data_t *state_data, uint8_t record_type)
       if (!ret)
         goto newline_out;
 
-      if ((ret = _normal_output_not_available_sensor_name_and_type (state_data)) < 0)
+      if ((ret = _normal_output_not_available_sensor_name (state_data)) < 0)
         goto cleanup;
 
       if (!ret)
         goto newline_out;
+
+      if (!state_data->prog_data->args->no_sensor_type_output)
+	{
+	  if ((ret = _normal_output_not_available_sensor_type (state_data)) < 0)
+	    goto cleanup;
+
+	  if (!ret)
+	    goto newline_out;
+	}
 
       if (state_data->prog_data->args->output_event_state)
         {
