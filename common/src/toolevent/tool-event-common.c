3101854f867edc01decdfa59d289fd320bcc3f57
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2011-10-31 17:04:24 +0000

    code cleanup ; begin making toolevent common lib

---
+++ common/src/toolevent/tool-event-common.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2003-2011 FreeIPMI Core Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#if STDC_HEADERS
+#include <string.h>
+#endif /* STDC_HEADERS */
+#include <assert.h>
+#include <errno.h>
+
+#include <freeipmi/freeipmi.h>
+
+#include "tool-event-common.h"
+
+#include "freeipmi-portability.h"
+#include "pstdout.h"
+
