e30ccdc5ec13c8839b75d4f1e3bd1e80e3589667
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2011-11-01 22:52:16 +0000

    Output line numbers when there is an error on file/stdin input.

--- ipmi-raw/src/ipmi-raw.c
+++ ipmi-raw/src/ipmi-raw.c
@@ -138,7 +138,8 @@ static int
 string2bytes (ipmi_raw_state_data_t *state_data,
               const char *line,
               unsigned char **buf,
-              unsigned int *len)
+              unsigned int *len,
+	      unsigned int line_count)
 {
   const char delim[] = " \t\f\v\r\n";
   char *str = NULL;
@@ -200,7 +201,8 @@ string2bytes (ipmi_raw_state_data_t *state_data,
                 {
                   pstdout_fprintf (state_data->pstate,
                                    stderr,
-                                   "invalid input\n");
+                                   "invalid input line: %u\n",
+				   line_count);
                   goto cleanup;
                 }
               l = strlen (token);
@@ -211,7 +213,8 @@ string2bytes (ipmi_raw_state_data_t *state_data,
         {
           pstdout_fprintf (state_data->pstate,
                            stderr,
-                           "invalid input\n");
+                           "invalid input line: %u\n",
+			   line_count);
           goto cleanup;
         }
 
@@ -221,7 +224,8 @@ string2bytes (ipmi_raw_state_data_t *state_data,
             {
               pstdout_fprintf (state_data->pstate,
                                stderr,
-                               "invalid input\n");
+                               "invalid input line: %u\n",
+			       line_count);
               goto cleanup;
             }
         }
@@ -233,7 +237,8 @@ string2bytes (ipmi_raw_state_data_t *state_data,
 	{
 	  pstdout_fprintf (state_data->pstate,
 			   stderr,
-			   "invalid input\n");
+			   "invalid input line: %u\n",
+			   line_count);
 	  goto cleanup;
 	}
       (*buf)[count++] = (unsigned char) value;
@@ -290,7 +295,7 @@ ipmi_raw_stream (ipmi_raw_state_data_t *state_data, FILE *stream)
        * a set of tasks.  We do not want to continue the set of tasks
        * if one in the middle is invalid.
        */
-      if (string2bytes (state_data, line, &bytes_rq, &send_len) < 0)
+      if (string2bytes (state_data, line, &bytes_rq, &send_len, line_count) < 0)
         goto cleanup;
 
       if (send_len <= 2)
