f8a7e9121820250545f64b05020f441cffa6c302
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2011-11-01 00:02:02 +0000

    code re-org

--- ipmi-sel/src/ipmi-sel.c
+++ ipmi-sel/src/ipmi-sel.c
@@ -1108,7 +1108,6 @@ _normal_output_event (ipmi_sel_state_data_t *state_data, unsigned int flags)
   uint8_t sensor_type;
   uint8_t event_data1_offset;
   int check_for_half_na = 0;
-  int read_oem_event_string = 0;
   int ret;
 
   assert (state_data);
@@ -1122,24 +1121,27 @@ _normal_output_event (ipmi_sel_state_data_t *state_data, unsigned int flags)
                                                    EVENT_OUTPUT_BUFLEN,
                                                    flags)) < 0)
         return (-1);
-
-      if (outbuf_len)
-        read_oem_event_string++;
+	
+	if (outbuf_len)
+	  {
+	    if (state_data->prog_data->args->comma_separated_output)
+	      pstdout_printf (state_data->pstate, ",%s", outbuf);
+	    else
+	      pstdout_printf (state_data->pstate, " | %s", outbuf);
+	    goto out;
+	  }
     }
-
-  if (!outbuf_len)
+  
+  memset (outbuf, '\0', EVENT_OUTPUT_BUFLEN+1);
+  if ((outbuf_len = ipmi_sel_parse_read_record_string (state_data->sel_parse_ctx,
+						       "%e",
+						       outbuf,
+						       EVENT_OUTPUT_BUFLEN,
+						       flags)) < 0)
     {
-      memset (outbuf, '\0', EVENT_OUTPUT_BUFLEN+1);
-      if ((outbuf_len = ipmi_sel_parse_read_record_string (state_data->sel_parse_ctx,
-                                                           "%e",
-                                                           outbuf,
-                                                           EVENT_OUTPUT_BUFLEN,
-                                                           flags)) < 0)
-        {
-          if (_sel_parse_err_handle (state_data, "ipmi_sel_parse_read_record_string") < 0)
-            return (-1);
-          return (0);
-        }
+      if (_sel_parse_err_handle (state_data, "ipmi_sel_parse_read_record_string") < 0)
+	return (-1);
+      return (0);
     }
 
   if (state_data->prog_data->args->comma_separated_output)
@@ -1156,11 +1158,7 @@ _normal_output_event (ipmi_sel_state_data_t *state_data, unsigned int flags)
       else
         pstdout_printf (state_data->pstate, " | %s", EVENT_NA_STRING);
     }
-
-  /* we're done if we got an OEM event string */
-  if (read_oem_event_string)
-    goto out;
-
+  
   if ((ret = _get_system_event_record_info (state_data,
                                             &event_type_code,
                                             &event_data2_flag,
