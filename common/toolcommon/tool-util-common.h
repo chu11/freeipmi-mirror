d7160ba6f89e205a383a634b8b8c05eaad48b675
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2012-05-16 00:20:32 +0000

    merge 8972:8973 trunk

---
+++ common/toolcommon/tool-util-common.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2003-2012 FreeIPMI Core Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef TOOL_UTIL_COMMON_H
+#define TOOL_UTIL_COMMON_H
+
+#include <stdint.h>
+#include <freeipmi/freeipmi.h>
+
+int ipmi_is_root ();
+
+void ipmi_disable_coredump (void);
+
+/* Check if kg len is decent */
+int check_kg_len (const char *in);
+
+/* Turn a 20-byte binary k_g key into an output string */
+char *format_kg (char *out, unsigned int outlen, const void *k_g);
+
+#endif /* TOOL_UTIL_COMMON_H */
