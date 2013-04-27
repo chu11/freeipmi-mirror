95e1ac58eb7a7bdb071dc11b7f87e905b7b70208
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2013-04-27 00:02:00 +0000

    New timestamp utility files

---
+++ libfreeipmi/util/ipmi-timestamp-util.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2003-2013 FreeIPMI Core Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <stdlib.h>
+#if STDC_HEADERS
+#include <string.h>
+#endif /* STDC_HEADERS */
+#if TIME_WITH_SYS_TIME
+#include <sys/time.h>
+#include <time.h>
+#else /* !TIME_WITH_SYS_TIME */
+#if HAVE_SYS_TIME_H
+#include <sys/time.h>
+#else /* !HAVE_SYS_TIME_H */
+#include <time.h>
+#endif /* !HAVE_SYS_TIME_H */
+#endif /* !TIME_WITH_SYS_TIME */
+#include <errno.h>
+
+#include "freeipmi/util/ipmi-timestamp-util.h"
+#include "freeipmi/spec/ipmi-timestamp-spec.h"
+
+#include "libcommon/ipmi-trace.h"
+
+#include "freeipmi-portability.h"
+
