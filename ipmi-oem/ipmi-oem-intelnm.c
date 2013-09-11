000a51186f69545dee923b0fe7ac3c6a1d5aa18c
chu11 <chu11@f7bdd6f0-feb3-4745-876d-a5d75e847258>
2013-09-11 23:49:15 +0000

    support new intel nm version numbers

--- ipmi-oem/ipmi-oem-intelnm.c
+++ ipmi-oem/ipmi-oem-intelnm.c
@@ -4768,6 +4768,12 @@ ipmi_oem_intelnm_get_node_manager_version (ipmi_oem_state_data_t *state_data)
   else if (node_manager_version == IPMI_OEM_INTEL_NODE_MANAGER_VERSION_1_5)
     pstdout_printf (state_data->pstate,
                     "Node Manager Version   : 1.5\n");
+  else if (node_manager_version == IPMI_OEM_INTEL_NODE_MANAGER_VERSION_2_0)
+    pstdout_printf (state_data->pstate,
+                    "Node Manager Version   : 2.0\n");
+  else if (node_manager_version == IPMI_OEM_INTEL_NODE_MANAGER_VERSION_2_5)
+    pstdout_printf (state_data->pstate,
+		    "Node Manager Version   : 2.5\n");
   else
     pstdout_printf (state_data->pstate,
                     "Node Manager Version   : %02Xh\n",
@@ -4776,6 +4782,9 @@ ipmi_oem_intelnm_get_node_manager_version (ipmi_oem_state_data_t *state_data)
   if (ipmi_interface_version == IPMI_OEM_INTEL_NODE_MANAGER_IPMI_INTERFACE_VERSION_1_0)
     pstdout_printf (state_data->pstate,
                     "IPMI Interface Version : 1.0\n");
+  else if (ipmi_interface_version == IPMI_OEM_INTEL_NODE_MANAGER_IPMI_INTERFACE_VERSION_2_0)
+    pstdout_printf (state_data->pstate,
+                    "IPMI Interface Version : 2.0\n");
   else
     pstdout_printf (state_data->pstate,
                     "IPMI Interface Version : %02Xh\n",
