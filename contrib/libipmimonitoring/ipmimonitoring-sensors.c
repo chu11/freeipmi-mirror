c6d339525077e43d2a6b3f2e7bf333fcd9d530ca
Albert Chu <chu11@llnl.gov>
2018-12-13 11:42:22 -0800

    contrib: Fix corner case libipmimonitoring-sensors output

--- contrib/libipmimonitoring/ipmimonitoring-sensors.c
+++ contrib/libipmimonitoring/ipmimonitoring-sensors.c
@@ -440,16 +440,11 @@ _ipmimonitoring (struct ipmi_monitoring_ipmi_config *ipmi_config)
                    ipmi_monitoring_ctx_errormsg (ctx));
           goto cleanup;
         }
-      else if (sensor_bitmask != IPMI_MONITORING_SENSOR_BITMASK_TYPE_UNKNOWN)
-        {
-          if (!(sensor_bitmask_strings = ipmi_monitoring_sensor_read_sensor_bitmask_strings (ctx)))
-            {
-              fprintf (stderr,
-                       "ipmi_monitoring_sensor_read_sensor_bitmask_strings: %s\n",
-                       ipmi_monitoring_ctx_errormsg (ctx));
-              goto cleanup;
-            }
-        }
+
+      /* it's ok for this to be NULL, i.e. sensor_bitmask ==
+       * IPMI_MONITORING_SENSOR_BITMASK_TYPE_UNKNOWN
+       */
+      sensor_bitmask_strings = ipmi_monitoring_sensor_read_sensor_bitmask_strings (ctx);
 
       if ((sensor_reading_type = ipmi_monitoring_sensor_read_sensor_reading_type (ctx)) < 0)
         {
