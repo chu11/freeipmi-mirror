d6f1f0a74268f120900d215172ec186b46131fda
Albert Chu <chu11@llnl.gov>
2019-02-08 18:20:27 -0800

    libfreeipmi/interpret: Support intepretation of system firmware progress + state sensors / events.

--- libfreeipmi/interpret/ipmi-interpret-config-sensor.c
+++ libfreeipmi/interpret/ipmi-interpret-config-sensor.c
@@ -536,6 +536,14 @@ static struct ipmi_interpret_sensor_config ipmi_interpret_sensor_system_firmware
   };
 static unsigned int ipmi_interpret_sensor_system_firmware_progress_config_len = 4;
 
+static struct ipmi_interpret_sensor_config ipmi_interpret_sensor_system_firmware_progress_state_config[] =
+  {
+    { "IPMI_System_Firmware_Progress_State_No_Event", IPMI_INTERPRET_STATE_NOMINAL},
+    { "IPMI_System_Firmware_Progress_State_Deasserted", IPMI_INTERPRET_STATE_NOMINAL},
+    { "IPMI_System_Firmware_Progress_State_Asserted", IPMI_INTERPRET_STATE_WARNING},
+  };
+static unsigned int ipmi_interpret_sensor_system_firmware_progress_state_config_len = 3;
+
 static struct ipmi_interpret_sensor_config ipmi_interpret_sensor_system_firmware_progress_device_present_config[] =
   {
     { "IPMI_System_Firmware_Progress_Device_Present_Device_No_Event", IPMI_INTERPRET_STATE_NOMINAL},
@@ -1830,6 +1838,12 @@ interpret_sensor_init (ipmi_interpret_ctx_t ctx)
     goto cleanup;
 
   if (_interpret_config_sensor_init (ctx,
+                                     &ctx->interpret_sensor.ipmi_interpret_sensor_system_firmware_progress_state_config,
+                                     ipmi_interpret_sensor_system_firmware_progress_state_config,
+                                     ipmi_interpret_sensor_system_firmware_progress_state_config_len) < 0)
+    goto cleanup;
+
+  if (_interpret_config_sensor_init (ctx,
                                      &ctx->interpret_sensor.ipmi_interpret_sensor_system_firmware_progress_device_present_config,
                                      ipmi_interpret_sensor_system_firmware_progress_device_present_config,
                                      ipmi_interpret_sensor_system_firmware_progress_device_present_config_len) < 0)
@@ -2186,6 +2200,9 @@ interpret_sensor_destroy (ipmi_interpret_ctx_t ctx)
 
   _interpret_config_sensor_destroy (ctx,
                                     ctx->interpret_sensor.ipmi_interpret_sensor_system_firmware_progress_config);
+
+  _interpret_config_sensor_destroy (ctx,
+                                    ctx->interpret_sensor.ipmi_interpret_sensor_system_firmware_progress_state_config);
 
   _interpret_config_sensor_destroy (ctx,
                                     ctx->interpret_sensor.ipmi_interpret_sensor_system_firmware_progress_device_present_config);
@@ -2562,6 +2579,7 @@ interpret_sensor_config_parse (ipmi_interpret_ctx_t ctx,
   int ipmi_interpret_sensor_drive_slot_device_present_flags[ipmi_interpret_sensor_drive_slot_device_present_config_len];
   int ipmi_interpret_sensor_post_memory_resize_state_flags[ipmi_interpret_sensor_post_memory_resize_state_config_len];
   int ipmi_interpret_sensor_system_firmware_progress_flags[ipmi_interpret_sensor_system_firmware_progress_config_len];
+  int ipmi_interpret_sensor_system_firmware_progress_state_flags[ipmi_interpret_sensor_system_firmware_progress_state_config_len];
   int ipmi_interpret_sensor_system_firmware_progress_device_present_flags[ipmi_interpret_sensor_system_firmware_progress_device_present_config_len];
   int ipmi_interpret_sensor_system_firmware_progress_transition_severity_flags[ipmi_interpret_sensor_system_firmware_progress_transition_severity_config_len];
   int ipmi_interpret_sensor_event_logging_disabled_flags[ipmi_interpret_sensor_event_logging_disabled_config_len];
@@ -2844,6 +2862,12 @@ interpret_sensor_config_parse (ipmi_interpret_ctx_t ctx,
                                ctx->interpret_sensor.ipmi_interpret_sensor_system_firmware_progress_config,
                                ipmi_interpret_sensor_system_firmware_progress_flags,
                                ipmi_interpret_sensor_system_firmware_progress_config_len);
+
+  _fill_sensor_config_options (config_file_options,
+                               &config_file_options_len,
+                               ctx->interpret_sensor.ipmi_interpret_sensor_system_firmware_progress_state_config,
+                               ipmi_interpret_sensor_system_firmware_progress_state_flags,
+                               ipmi_interpret_sensor_system_firmware_progress_state_config_len);
 
   _fill_sensor_config_options (config_file_options,
                                &config_file_options_len,
