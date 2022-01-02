4844ef8b9dfc8f326bb09408bdbe1b1f44ab9e0a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-02 10:37:00 +0100

    check for inb and outb
    Check for inb and outb before using them to avoid the following build
    failure on uclibc-ng:
    
    /home/buildroot/autobuild/instance-1/output-1/host/opt/ext-toolchain/bin/../lib/gcc/powerpc-buildroot-linux-uclibc/10.3.0/../../../../powerpc-buildroot-linux-uclibc/bin/ld: ../libfreeipmi/.libs/libfreeipmi.a(libfreeipmi_la-ipmi-kcs-driver.o): in function `_ipmi_kcs_test_if_state':
    /home/buildroot/autobuild/instance-1/output-1/build/freeipmi-1.6.8/libfreeipmi/driver/ipmi-kcs-driver.c:537: undefined reference to `inb'
    
    Fixes:
     - http://autobuild.buildroot.org/results/13f58266f7747c183033c811a4e8ca6fa30b6f4d
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>

--- libfreeipmi/driver/ipmi-kcs-driver.c
+++ libfreeipmi/driver/ipmi-kcs-driver.c
@@ -162,7 +162,7 @@
 #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
 # define _INB(port)  inb (port)
 # define _OUTB(data, port)  outb (port, data)
-#elif defined(HAVE_IOPL)
+#elif defined(HAVE_INB) && defined(HAVE_OUTB)
 # define _INB(port)  inb (port)
 # define _OUTB(data, port)  outb (data, port)
 #else
